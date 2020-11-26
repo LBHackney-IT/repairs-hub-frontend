795167636d6e4dd06aac438bc31a625feb4411b4
Ben <benbaumann95@hotmail.co.uk>
2020-11-26 16:34:32 +0000

    Add Hackney header component - Adds some initial config for jest testing setup

---
+++ components/Layout/Header/Header.js
@@ -0,0 +1,11 @@
+import { Header } from 'lbh-frontend-react'
+
+const HeaderComponent = ({ serviceName }) => (
+  <div>
+    <Header
+      serviceName={serviceName} isServiceNameShort={false} homepageUrl="/"
+    />
+  </div>
+);
+
+export default HeaderComponent
