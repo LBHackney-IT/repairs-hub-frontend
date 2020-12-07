ba8e51e2cd94781702dc817c54c63dbf273d5ab2
Ben <benbaumann95@hotmail.co.uk>
2020-12-07 13:18:45 +0000

    Add IP whitelisting for app using env

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import '../styles/all.scss'
 import App from 'next/app'
 import Layout from '../components/Layout'
 
+const { NEXT_PUBLIC_ALLOWED_IP_ADDRESSES } = process.env
+
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
@@ -12,6 +14,31 @@ class MyApp extends App {
         </Layout>
       </>
     )
+  }
+}
+
+if (NEXT_PUBLIC_ALLOWED_IP_ADDRESSES) {
+  MyApp.getInitialProps = async ({ ctx }) => {
+    // Block access by default
+    let shouldBlockAccess = true
+
+    // Specify IP's to whitelist
+    var allowedIps = NEXT_PUBLIC_ALLOWED_IP_ADDRESSES.split(",")
+    // Read the request objects headers to find out ip address
+    var ip = ctx.req.headers['x-forwarded-for'] || ctx.req.connection.remoteAddress
+
+    if (allowedIps.includes(ip)) {
+      shouldBlockAccess = false
+    }
+
+    // Return the 403 status code
+    if (shouldBlockAccess) {
+      ctx.res.statusCode = 403
+      ctx.res.end(`Not allowed for ${ip}`)
+      return
+    }
+
+    return { shouldBlockAccess, ip }
   }
 }
 
