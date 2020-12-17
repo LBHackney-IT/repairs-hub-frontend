c3144a5d1ed0bf854aca53eb4e2a8ded962db5ab
Ben <benbaumann95@hotmail.co.uk>
2020-12-17 11:25:14 +0000

    Split search for property and show property integration specs

--- cypress/integration/search_for_property.spec.js
+++ cypress/integration/search_for_property.spec.js
@@ -8,8 +8,6 @@ beforeEach(() => {
 
 describe('Search for property', () => {
   it('Search for property by postcode', () => {
-    cy.fixture('properties/property.json').as('property')
-
     // Stub request for search on properties by postcode
     cy.route('GET', 'api/v2/properties/?q=e9 6pt', '@propertiesList')
 
@@ -29,37 +27,11 @@ describe('Search for property', () => {
       cy.contains('th', 'Property reference')
     })
 
-    // Stub request for property response
-    cy.route('GET', 'api/v2/properties/00012345', '@property')
-
-    // Click property with reference 00012345
-    cy.get('.govuk-table__cell a').first().click()
-    cy.url().should('contains', 'properties/00012345')
-
-    cy.get('.govuk-heading-l').contains('Dwelling: 16 Pitcairn House')
-
-    // Property details
-    cy.get('.govuk-grid-row').within(() => {
-      cy.contains('Property details')
-      cy.contains('16 Pitcairn House')
-      cy.contains('St Thomass Square')
-      cy.contains('E9 6PT')
-    })
-
-    // Tenure (raisable repair)
-    cy.get('.hackney-property-alerts li.bg-turquoise').within(() => {
-      cy.contains('Tenure: Secure')
-    })
-
-    // Alerts
-    cy.get('.hackney-property-alerts').within(() => {
-      // Location alerts
-      cy.contains('Address Alert: Property Under Disrepair (DIS)')
-
-      // Person alerts
-      cy.contains('Contact Alert: No Lone Visits (CV)')
-      cy.contains('Contact Alert: Verbal Abuse or Threat of (VA)')
-    })
+    cy.get('.govuk-table__cell a').should(
+      'have.attr',
+      'href',
+      '/properties/00012345'
+    )
   })
 
   it('Search for property by address', () => {
@@ -73,31 +45,5 @@ describe('Search for property', () => {
     cy.get('.govuk-heading-s').contains(
       'We found 2 matching results for: pitcairn'
     )
-  })
-
-  it('Search for property with unraisable repair', () => {
-    cy.fixture('properties/property_repair_not_raisable.json').as('property')
-
-    // Stub request for property with unraisable tenure code
-    cy.route('GET', 'api/v2/properties/00012345', '@property')
-
-    cy.visit('properties/00012345')
-
-    // Tenure (not raisable repair)
-    cy.get('.hackney-property-alerts li.bg-orange').within(() => {
-      cy.contains('Tenure: Leasehold (RTB)')
-    })
-  })
-
-  it('Search for property with no tenure information', () => {
-    cy.fixture('properties/property_no_tenure.json').as('property')
-
-    // Stub request for property with unraisable tenure code
-    cy.route('GET', 'api/v2/properties/00012345', '@property')
-
-    cy.visit('properties/00012345')
-
-    cy.get('.govuk-heading-l').contains('Dwelling: 16 Pitcairn House')
-    cy.contains('Tenure').should('not.exist')
   })
 })
---
+++ cypress/integration/show_property.spec.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+beforeEach(() => {
+  cy.server()
+})
+
+describe('Show property', () => {
+  it('Display property details, tenure and alerts', () => {
+    // Stub request with property response
+    cy.fixture('properties/property.json').as('property')
+    cy.route('GET', 'api/v2/properties/00012345', '@property')
+    cy.visit('properties/00012345')
+
+    // Property details
+    cy.get('.govuk-heading-l').contains('Dwelling: 16 Pitcairn House')
+    cy.get('.govuk-grid-row').within(() => {
+      cy.contains('Property details')
+      cy.contains('16 Pitcairn House')
+      cy.contains('St Thomass Square')
+      cy.contains('E9 6PT')
+    })
+
+    // Tenure (raisable repair)
+    cy.get('.hackney-property-alerts li.bg-turquoise').within(() => {
+      cy.contains('Tenure: Secure')
+    })
+
+    // Alerts
+    cy.get('.hackney-property-alerts').within(() => {
+      // Location alerts
+      cy.contains('Address Alert: Property Under Disrepair (DIS)')
+
+      // Person alerts
+      cy.contains('Contact Alert: No Lone Visits (CV)')
+      cy.contains('Contact Alert: Verbal Abuse or Threat of (VA)')
+    })
+  })
+
+  it('Display property with a tenure type that does not permit raising a repair', () => {
+    // Stub request with property response
+    cy.fixture('properties/property_repair_not_raisable.json').as('property')
+    cy.route('GET', 'api/v2/properties/00012345', '@property')
+    cy.visit('properties/00012345')
+
+    // Tenure (not raisable repair)
+    cy.get('.hackney-property-alerts li.bg-orange').within(() => {
+      cy.contains('Tenure: Leasehold (RTB)')
+    })
+  })
+
+  it('Display property with no tenure type', () => {
+    // Stub request with property response
+    cy.fixture('properties/property_no_tenure.json').as('property')
+    cy.route('GET', 'api/v2/properties/00012345', '@property')
+    cy.visit('properties/00012345')
+
+    cy.get('.govuk-heading-l').contains('Dwelling: 16 Pitcairn House')
+    cy.contains('Tenure').should('not.exist')
+  })
+})
