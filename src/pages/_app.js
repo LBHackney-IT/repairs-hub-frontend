ee4bf9e5cd29de45887bbc685e09774da7a9cc68
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-01-26 19:25:59 +0000

    Remove IP blocking now we have auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,8 +6,6 @@ import Head from 'next/head'
 import { isAuthorised, AUTH_WHITELIST } from '../utils/GoogleAuth'
 import UserContext from '../components/UserContext/UserContext'
 
-const { NEXT_PUBLIC_ALLOWED_IP_ADDRESSES } = process.env
-
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
@@ -35,31 +33,7 @@ MyApp.getInitialProps = async ({ ctx }) => {
 
   const userDetails = isAuthorised(ctx, WITH_REDIRECT)
 
-  if (NEXT_PUBLIC_ALLOWED_IP_ADDRESSES) {
-    // Block access by default
-    let shouldBlockAccess = true
-
-    // Specify IP's to whitelist
-    var allowedIps = NEXT_PUBLIC_ALLOWED_IP_ADDRESSES.split(',')
-    // Read the request objects headers to find out ip address
-    var ip =
-      ctx.req.headers['x-forwarded-for'] || ctx.req.connection.remoteAddress
-
-    if (allowedIps.includes(ip)) {
-      shouldBlockAccess = false
-    }
-
-    // Return the 403 status code
-    if (shouldBlockAccess) {
-      ctx.res.statusCode = 403
-      ctx.res.end(`Not allowed for ${ip}`)
-      return
-    }
-
-    return { userDetails, shouldBlockAccess, ip }
-  } else {
-    return { userDetails }
-  }
+  return { userDetails }
 }
 
 export default MyApp
