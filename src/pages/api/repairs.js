07e1098420dadc411f08f70da5ab3ba0c198654d
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-02-08 15:52:45 +0000

    Extract user token in service API wrapper function
    Now we're calling this from more places it seems to make sense to
    let this function do the cookie parsing in one place.

--- src/pages/api/repairs.js
+++ src/pages/api/repairs.js
@@ -1,5 +1,4 @@
 import * as HttpStatus from 'http-status-codes'
-import cookie from 'cookie'
 
 import { CONTRACTOR_ROLE } from '../../utils/user'
 import {
@@ -7,12 +6,7 @@ import {
   authoriseServiceAPIRequest,
 } from '../../utils/service-api-client'
 
-const { GSSO_TOKEN_NAME } = process.env
-
 export default authoriseServiceAPIRequest(async (req, res, user) => {
-  const cookies = cookie.parse(req.headers.cookie ?? '')
-  const token = cookies[GSSO_TOKEN_NAME]
-
   // Inject contractor query param for repair index requests
   if (req.method.toLowerCase() === 'get' && user.hasRole(CONTRACTOR_ROLE)) {
     req.query = {
@@ -23,7 +17,7 @@ export default authoriseServiceAPIRequest(async (req, res, user) => {
 
   req.query = { ...req.query, path: ['repairs'] }
 
-  const data = await serviceAPIRequest(req, token)
+  const data = await serviceAPIRequest(req)
 
   res.status(HttpStatus.OK).json(data)
 })
