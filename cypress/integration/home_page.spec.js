0250b96e4461b5ff2c51c833a16fb643e3898b1e
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-02-17 12:45:12 +0000

    Test both role's access in home_page.spec
    Previously the contractor tests lived in a separate file, but I think
    this change makes it easier for us to be sure that the home page
    conditional view logic per role is working correctly.

--- cypress/integration/home_page.spec.js
+++ cypress/integration/home_page.spec.js
@@ -3,12 +3,30 @@
 import 'cypress-audit/commands'
 
 describe('Home page', () => {
-  context('When user is logged in', () => {
+  context('When user is not logged in', () => {
+    it('Redirects on the login page', () => {
+      cy.visit(Cypress.env('HOST'))
+      // Header component
+      cy.get('.lbh-header__service-name').contains('Repairs Hub')
+      cy.get('.lbh-header__title-link').should('have.attr', 'href', '/')
+
+      // UserLogin component
+      cy.get('.govuk-heading-m').contains('Login')
+      cy.get('.govuk-body').contains(
+        'Please log in with an approved Hackney email account.'
+      )
+
+      // Run lighthouse audit for accessibility report
+      cy.audit()
+    })
+  })
+
+  context('When an agent is logged in', () => {
     beforeEach(() => {
       cy.loginWithAgentRole()
     })
 
-    it('Displays all necessary components on the home page', () => {
+    it('Shows the search page', () => {
       // Header component
       cy.get('.lbh-header__service-name').contains('Repairs Hub')
       cy.get('.lbh-header__title-link').should('have.attr', 'href', '/')
@@ -45,21 +63,70 @@ describe('Home page', () => {
     })
   })
 
-  context('When user is not logged in', () => {
-    it('Redirects on the login page', () => {
-      cy.visit(Cypress.env('HOST'))
-      // Header component
-      cy.get('.lbh-header__service-name').contains('Repairs Hub')
-      cy.get('.lbh-header__title-link').should('have.attr', 'href', '/')
+  context('When a contractor is logged in', () => {
+    describe('Show all work orders', () => {
+      context(
+        'Displays work order details, reference, date raised, last update, priority, property and description',
+        () => {
+          //Stub request with work orders response
+          beforeEach(() => {
+            cy.loginWithContractorRole()
+            cy.server()
+            cy.fixture('work_orders/jobs.json').as('workorderslist')
+            cy.route(
+              'GET',
+              'api/repairs/?PageSize=10&PageNumber=1',
+              '@workorderslist'
+            )
+            cy.visit(`${Cypress.env('HOST')}/`)
+          })
 
-      // UserLogin component
-      cy.get('.govuk-heading-m').contains('Login')
-      cy.get('.govuk-body').contains(
-        'Please log in with an approved Hackney email account.'
+          it('displays headers of the table', () => {
+            cy.get('.govuk-table').within(() => {
+              cy.contains('th', 'Reference')
+              cy.contains('th', 'Date raised')
+              cy.contains('th', 'Last update')
+              cy.contains('th', 'Priority')
+              cy.contains('th', 'Property')
+              cy.contains('th', 'Description')
+            })
+            // Run lighthouse audit for accessibility report
+            cy.audit()
+          })
+
+          it('displays reference number, date raised, last updated, priority, property address, description', () => {
+            cy.get('.govuk-table__cell').within(() => {
+              cy.contains('00012346')
+              cy.contains('13 Jan 2021')
+              cy.contains('14 Jan 2021')
+              cy.contains('N - Normal')
+              cy.contains('1 Pitcairn House St Thomass Square')
+              cy.contains(
+                'ALPHA- Pitcairn house op stucl behind carpark gates from power network pls remedy AND Communal: Door entry; Residents locked out/in'
+              )
+              cy.contains('a', 'Update')
+            })
+            // Run lighthouse audit for accessibility report
+            cy.audit()
+          })
+
+          it('does displays next button', () => {
+            cy.get('.page-navigation').within(() => {
+              cy.contains('Next')
+            })
+            // Run lighthouse audit for accessibility report
+            cy.audit()
+          })
+
+          it('does not displays previous button', () => {
+            cy.get('.page-navigation').within(() => {
+              cy.contains('Previous').should('not.exist')
+            })
+            // Run lighthouse audit for accessibility report
+            cy.audit()
+          })
+        }
       )
-
-      // Run lighthouse audit for accessibility report
-      cy.audit()
     })
   })
 })
