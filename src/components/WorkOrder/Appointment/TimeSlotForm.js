f65a305c951ea1d48057de2abeb5bbf313d881f3
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-02-24 16:14:03 +0000

    Improve layout of time slot form
    Includes:
      - Use SCSS variables and a calculation to set form width to match
        calendar at different breakpoints.
      - Set date text font

--- src/components/WorkOrder/Appointment/TimeSlotForm.js
+++ src/components/WorkOrder/Appointment/TimeSlotForm.js
@@ -14,51 +14,50 @@ const TimeSlotForm = ({
   const { handleSubmit, register, errors } = useForm({})
 
   return (
-    <div className="appointment-am-pm-form">
+    <div className="govuk-grid-row">
       <div className="govuk-grid-column-full">
         <form
           role="form"
           id="appointment"
+          className="timeslot-form govuk-!-padding-4"
           onSubmit={handleSubmit(onGetToSummary)}
         >
-          <div className="am-pm-slots-padding">
-            <h2>{date}</h2>
-            <Radios
-              label=""
-              name="options"
-              options={availableSlots['slots'].map((slot) => {
-                let text =
-                  slot['description'] == 'AM Slot'
-                    ? 'AM 8:00 -12:00'
-                    : 'PM 12:00-4:00'
-                return {
-                  text: text,
-                  value: text,
-                  defaultChecked: text == timeSlot,
-                }
-              })}
-              register={register({ required: 'Please select a time slot' })}
-              error={errors && errors.options}
-            />
-            <TextArea
-              className="appointment-text-area"
-              name="comments"
-              label="Comments"
-              register={register({
-                required: 'Please add comments',
-              })}
-              error={errors && errors.comments}
-              defaultValue={comments}
-            />
-            <Button
-              type="button"
-              isSecondary={true}
-              label="Cancel"
-              onClick={onCancel}
-              className="lbh-button--repairs govuk-!-margin-right-4"
-            />
-            <Button label="Add" type="submit" className="lbh-button--repairs" />
-          </div>
+          <h2 className="govuk-heading-s">{date}</h2>
+          <Radios
+            label=""
+            name="options"
+            options={availableSlots['slots'].map((slot) => {
+              let text =
+                slot['description'] == 'AM Slot'
+                  ? 'AM 8:00 -12:00'
+                  : 'PM 12:00-4:00'
+              return {
+                text: text,
+                value: text,
+                defaultChecked: text == timeSlot,
+              }
+            })}
+            register={register({ required: 'Please select a time slot' })}
+            error={errors && errors.options}
+          />
+          <TextArea
+            className="appointment-text-area"
+            name="comments"
+            label="Comments"
+            register={register({
+              required: 'Please add comments',
+            })}
+            error={errors && errors.comments}
+            defaultValue={comments}
+          />
+          <Button
+            type="button"
+            isSecondary={true}
+            label="Cancel"
+            onClick={onCancel}
+            className="lbh-button--repairs govuk-!-margin-right-4"
+          />
+          <Button label="Add" type="submit" className="lbh-button--repairs" />
         </form>
       </div>
     </div>
