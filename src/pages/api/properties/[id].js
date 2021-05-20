4aa0b0ce578db0dc82f8201698120f5d3ecd2ed1
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-05-20 11:23:12 +0100

    Do not redact contacts for auth managers
    This caused an app error when auth managers attempted repair raising
    because the raise repair form expects an array of contacts.

--- src/pages/api/properties/[id].js
+++ src/pages/api/properties/[id].js
@@ -1,5 +1,9 @@
 import * as HttpStatus from 'http-status-codes'
-import { AGENT_ROLE, CONTRACT_MANAGER_ROLE } from '../../../utils/user'
+import {
+  AGENT_ROLE,
+  AUTHORISATION_MANAGER_ROLE,
+  CONTRACT_MANAGER_ROLE,
+} from '../../../utils/user'
 import {
   serviceAPIRequest,
   authoriseServiceAPIRequest,
@@ -11,7 +15,11 @@ export default authoriseServiceAPIRequest(async (req, res, user) => {
   const data = await serviceAPIRequest(req)
 
   // redact contact information for contractor responses
-  if (user.hasRole(AGENT_ROLE) || user.hasRole(CONTRACT_MANAGER_ROLE)) {
+  if (
+    user.hasRole(AGENT_ROLE) ||
+    user.hasRole(CONTRACT_MANAGER_ROLE) ||
+    user.hasRole(AUTHORISATION_MANAGER_ROLE)
+  ) {
     res.status(HttpStatus.OK).json(data)
   } else {
     res.status(HttpStatus.OK).json({ ...data, contacts: '[REMOVED]' })
--- src/pages/api/properties/[id].test.js
+++ src/pages/api/properties/[id].test.js
@@ -13,6 +13,7 @@ const {
   GSSO_TOKEN_NAME,
   AGENTS_GOOGLE_GROUPNAME,
   REPAIRS_SERVICE_API_KEY,
+  AUTHORISATION_MANAGERS_GOOGLE_GROUPNAME,
 } = process.env
 
 describe('GET /api/properties/[id] contact information redaction', () => {
@@ -51,6 +52,65 @@ describe('GET /api/properties/[id] contact information redaction', () => {
         name: 'name',
         email: 'name@example.com',
         groups: [AGENTS_GOOGLE_GROUPNAME],
+      },
+      HACKNEY_JWT_SECRET
+    )
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'x-api-key': REPAIRS_SERVICE_API_KEY,
+      'x-hackney-user': signedCookie,
+    }
+
+    test('the response can include full contact information', async () => {
+      const req = createRequest({
+        method: 'get',
+        headers: { Cookie: `${GSSO_TOKEN_NAME}=${signedCookie};` },
+        query: {
+          id: '1',
+        },
+        params: {},
+      })
+
+      const res = createResponse()
+
+      await propertiesEndpoint(req, res)
+
+      expect(axios).toHaveBeenCalledTimes(1)
+      expect(axios).toHaveBeenCalledWith({
+        method: 'get',
+        headers,
+        url: `${REPAIRS_SERVICE_API_URL}/properties/1`,
+        params: {},
+        paramsSerializer: paramsSerializer,
+        data: {},
+      })
+
+      const parsedData = JSON.parse(res._getData())
+
+      expect(parsedData['contacts']).toEqual({
+        contacts: [
+          {
+            firstName: 'FirstName',
+            lastName: 'LastName',
+            phoneNumbers: ['0123456789', '0123456789'],
+          },
+          {
+            firstName: 'FirstName',
+            lastName: 'LastName',
+            phoneNumbers: ['0123456789', '0123456789'],
+          },
+        ],
+      })
+    })
+  })
+
+  describe('when the cookie is for an authorisation manager', () => {
+    const signedCookie = jsonwebtoken.sign(
+      {
+        name: 'name',
+        email: 'name@example.com',
+        groups: [AUTHORISATION_MANAGERS_GOOGLE_GROUPNAME],
       },
       HACKNEY_JWT_SECRET
     )
