d86be30cc39ae18f6cb60c8db67cd106b0a379fd
Evangelos Giataganas <e.giataganas@gmail.com>
2021-05-28 15:35:55 +0100

    Update layout to match Hackney design system

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,17 @@
 import HeaderComponent from './Header/Header'
 
 const Layout = ({ children }) => (
-  <div>
+  <>
     <HeaderComponent serviceName="Repairs Hub" />
 
-    <div className="govuk-width-container">
-      <main
-        className="govuk-main-wrapper repairs-hub-main-wrapper"
-        id="main-content"
-        role="main"
-      >
-        {children}
-      </main>
-    </div>
-  </div>
+    <main
+      className="lbh-main-wrapper repairs-hub-main-wrapper"
+      id="main-content"
+      role="main"
+    >
+      <div className="lbh-container">{children}</div>
+    </main>
+  </>
 )
 
 export default Layout
