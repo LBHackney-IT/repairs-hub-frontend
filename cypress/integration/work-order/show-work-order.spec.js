4e800ec9eb3b71bf52fe46e340b5fd8f0cb64644
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-06-08 17:27:18 +0100

    Use more focused work order show page tests
    Adds shorter integration tests focused on the specific behavioural
    differences that we expect to occur for different contexts on this page.
    
    Also:
     - Adds a unit-level test for a child of the high level work
     order component to check for content differences at that level

--- cypress/integration/work-order/show-work-order.spec.js
+++ cypress/integration/work-order/show-work-order.spec.js
@@ -2,164 +2,106 @@
 
 import 'cypress-audit/commands'
 
-describe('Show work order', () => {
+describe('Show work order page', () => {
   beforeEach(() => {
     cy.loginWithAgentRole()
     cy.server()
-    // Stub request for work order and property
 
     cy.fixture('properties/property.json').as('property')
     cy.fixture('work-orders/notes.json').as('notes')
     cy.route('GET', 'api/properties/00012345', '@property')
     cy.route('GET', 'api/workOrders/10000012/notes', '@notes')
+
+    cy.fixture('work-orders/work-order.json').as('workOrder')
+    cy.route('GET', 'api/workOrders/10000012', '@workOrder')
+    cy.visit(`${Cypress.env('HOST')}/work-orders/10000012`)
   })
 
-  context('Displays the page for a work order with appointment details', () => {
+  it('Shows various details about the work order, property and assigned contractor', () => {
+    cy.get('.lbh-heading-h1').within(() => {
+      cy.contains('Works order: 10000012')
+    })
+
+    cy.get('.lbh-body-m').within(() => {
+      cy.contains('This is an urgent repair description')
+    })
+
+    cy.get('.property-details-main-section').within(() => {
+      cy.contains('Dwelling')
+      cy.contains('16 Pitcairn House').should(
+        'have.attr',
+        'href',
+        '/properties/00012345'
+      )
+      cy.contains('St Thomass Square').should(
+        'have.attr',
+        'href',
+        '/properties/00012345'
+      )
+      cy.contains('E9 6PT')
+    })
+
+    cy.checkForTenureAlertDetails(
+      'Tenure: Secure',
+      ['Address Alert: Property Under Disrepair (DIS)'],
+      [
+        'Contact Alert: No Lone Visits (CV)',
+        'Contact Alert: Verbal Abuse or Threat of (VA)',
+      ]
+    )
+
+    cy.get('.work-order-info').within(() => {
+      cy.contains('Status: In Progress')
+      cy.contains('Priority: U - Urgent (5 Working days)')
+      cy.contains('Raised by Dummy Agent')
+      cy.contains('18 Jan 2021, 3:28 pm')
+      cy.contains('Target: 23 Jan 2021, 6:30 pm')
+      cy.contains('Caller: Jill Smith')
+      cy.contains('07700 900999')
+    })
+
+    cy.contains('Assigned to: Alphatrack (S) Systems Lt')
+
+    cy.audit()
+  })
+
+  context('When the work order has no appointment', () => {
+    it('Shows a link to schedule it', () => {
+      cy.get('.appointment-details').within(() => {
+        cy.contains('Appointment details')
+        cy.contains('a', 'Schedule an appointment')
+      })
+    })
+  })
+
+  context('When the work order has an existing appointment', () => {
     beforeEach(() => {
       cy.fixture('work-orders/with-appointment.json').as('workOrder')
       cy.route('GET', 'api/workOrders/10000012', '@workOrder')
       cy.visit(`${Cypress.env('HOST')}/work-orders/10000012`)
     })
 
-    it('Works order header with reference number', () => {
-      cy.get('.lbh-heading-h1').within(() => {
-        cy.contains('Works order: 10000012')
-      })
-    })
-
-    it('Repair description', () => {
-      cy.get('.lbh-body-m').within(() => {
-        cy.contains('This is an urgent repair description')
-      })
-    })
-
-    it('Property details', () => {
-      cy.get('.property-details-main-section').within(() => {
-        cy.contains('Dwelling')
-        cy.contains('16 Pitcairn House').should(
-          'have.attr',
-          'href',
-          '/properties/00012345'
-        )
-        cy.contains('St Thomass Square').should(
-          'have.attr',
-          'href',
-          '/properties/00012345'
-        )
-        cy.contains('E9 6PT')
-      })
-
-      cy.checkForTenureAlertDetails(
-        'Tenure: Secure',
-        ['Address Alert: Property Under Disrepair (DIS)'],
-        [
-          'Contact Alert: No Lone Visits (CV)',
-          'Contact Alert: Verbal Abuse or Threat of (VA)',
-        ]
-      )
-
-      // Run lighthouse audit for accessibility report
-      cy.audit()
-    })
-
-    it('Work order details', () => {
-      cy.get('.work-order-info').within(() => {
-        cy.contains('Status: In Progress')
-        cy.contains('Priority: U - Urgent (5 Working days)')
-        cy.contains('Raised by Dummy Agent')
-        cy.contains('18 Jan 2021, 3:28 pm')
-        cy.contains('Target: 23 Jan 2021, 6:30 pm')
-        cy.contains('Caller: Jill Smith')
-        cy.contains('07700 900999')
-      })
-
-      // Run lighthouse audit for accessibility report
-      cy.audit()
-    })
-
-    it('Appointment details', () => {
+    it('Shows the scheduled appointment details', () => {
       cy.get('.appointment-details').within(() => {
         cy.contains('Appointment details')
         cy.contains('19 Mar 2021, 12:00-18:00')
       })
-      //can see who was assigned
-      cy.contains('Assigned to: Alphatrack (S) Systems Lt')
-
-      // Run lighthouse audit for accessibility report
-      cy.audit()
     })
   })
-  context(
-    'Displays the page for a work order without appointment details',
-    () => {
-      beforeEach(() => {
-        cy.fixture('work-orders/work-order.json').as('workOrder')
-        cy.route('GET', 'api/workOrders/10000012', '@workOrder')
-        cy.visit(`${Cypress.env('HOST')}/work-orders/10000012`)
-      })
 
-      it('Work order details', () => {
-        cy.get('.work-order-info').within(() => {
-          cy.contains('Status: In Progress')
-          cy.contains('Priority: U - Urgent (5 Working days)')
-          cy.contains('Raised by Dummy Agent')
-          cy.contains('18 Jan 2021, 3:28 pm')
-          cy.contains('Target: 23 Jan 2021, 6:30 pm')
-          cy.contains('Caller: Jill Smith')
-          cy.contains('07700 900999')
-        })
-
-        // Run lighthouse audit for accessibility report
-        cy.audit()
-      })
-
-      it('Appointment details', () => {
-        cy.get('.appointment-details').within(() => {
-          cy.contains('Appointment details')
-          cy.contains('a', 'Schedule an appointment')
-        })
-        //can see who was assigned
-        cy.contains('Assigned to: Alphatrack (S) Systems Lt')
-
-        // Run lighthouse audit for accessibility report
-        cy.audit()
-      })
-    }
-  )
-
-  context('Displays the page for a work order with Emergency priority', () => {
+  context('When the work order has an immediate priority', () => {
     beforeEach(() => {
-      cy.fixture('work-orders/priority-emergency.json').as('workOrderEmergency')
-      cy.route('GET', 'api/workOrders/10000012', '@workOrderEmergency')
+      cy.fixture('work-orders/priority-immediate.json').as('workOrderImmediate')
+      cy.route('GET', 'api/workOrders/10000012', '@workOrderImmediate')
 
       cy.visit(`${Cypress.env('HOST')}/work-orders/10000012`)
     })
 
-    it('Work order details', () => {
-      cy.get('.work-order-info').within(() => {
-        cy.contains('Status: In Progress')
-        cy.contains('Priority: 2 [E] EMERGENCY')
-        cy.contains('Raised by Dummy Agent')
-        cy.contains('18 Jan 2021, 3:28 pm')
-        cy.contains('Target: 23 Jan 2021, 6:30 pm')
-        cy.contains('Caller: Jill Smith')
-        cy.contains('07700 900999')
-      })
-
-      // Run lighthouse audit for accessibility report
-      cy.audit()
-    })
-
-    it('Appointment details', () => {
+    it('Indicates that an appointment is not applicable', () => {
       cy.get('.appointment-details').within(() => {
         cy.contains('Appointment details')
         cy.contains('Not applicable')
       })
-      //can see who was assigned
-      cy.contains('Assigned to: Alphatrack (S) Systems Lt')
-
-      // Run lighthouse audit for accessibility report
-      cy.audit()
     })
   })
 })
