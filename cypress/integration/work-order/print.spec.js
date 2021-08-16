d96a3434ec9bd97645e503e65944b741382dd6b8
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-08-16 18:14:26 +0100

    Simple print option detection test

---
+++ cypress/integration/work-order/print.spec.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+import 'cypress-audit/commands'
+
+describe('Printing', () => {
+  beforeEach(() => {
+    cy.loginWithContractorRole()
+
+    cy.intercept(
+      { method: 'GET', path: '/api/workOrders/10000012' },
+      { fixture: 'workOrders/workOrder.json' }
+    )
+    cy.intercept(
+      { method: 'GET', path: '/api/properties/00012345' },
+      { fixture: 'properties/property.json' }
+    )
+    cy.intercept(
+      { method: 'GET', path: '/api/workOrders/10000012/notes' },
+      { fixture: 'workOrders/notes.json' }
+    )
+    cy.intercept(
+      { method: 'GET', path: '/api/workOrders/10000012/tasks' },
+      { body: [] }
+    )
+    cy.intercept(
+      {
+        method: 'GET',
+        path: '/api/workOrders?propertyReference=00012345*',
+      },
+      { body: [] }
+    )
+  })
+
+  it('Shows a print option for a work order', () => {
+    cy.visit('/work-orders/10000012')
+
+    cy.get('[data-testid=details] > .govuk-button').click()
+    cy.get('[data-testid="details"]').contains('Print').click({ force: true })
+
+    cy.contains('[href$=print]', 'Print')
+  })
+})
