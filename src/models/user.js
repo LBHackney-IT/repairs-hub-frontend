759950cd64ef22903b287b473544fe71b9c5557d
Evangelos Giataganas <e.giataganas@gmail.com>
2021-08-27 15:05:45 +0100

    Set authServiceGroups as private variable

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 export class User {
+  #authServiceGroups
   constructor(name, email, authServiceGroups) {
     this.name = name
     this.email = email
-    this.authServiceGroups = authServiceGroups
+    this.#authServiceGroups = authServiceGroups
   }
 }
