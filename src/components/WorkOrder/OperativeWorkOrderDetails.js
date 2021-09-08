20644abfbf8d7886d6673cc09c55c19227bea910
Sia <anastasia.pankina@unboxedconsulting.com>
2021-09-08 21:06:04 +0100

    Add operative-index page and start building view page

---
+++ src/components/WorkOrder/OperativeWorkOrderDetails.js
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types'
+import WorkOrderHeader from './WorkOrderHeader'
+import BackButton from '../Layout/BackButton/BackButton'
+import { longMonthWeekday } from '../../utils/date'
+import { WorkOrder } from '../../models/workOrder'
+
+const WorkOrderDetails = ({
+  property,
+  workOrder,
+  locationAlerts,
+  personAlerts,
+  tenure,
+  schedulerSessionId,
+}) => {
+  return (
+    <>
+      <div class="govuk-panel govuk-panel--confirmation lbh-panel">
+        <h2 class="govuk-panel__title">
+          {longMonthWeekday(workOrder.appointment.date)}{' '}
+          {workOrder.appointment.start}-{workOrder.appointment.end}
+        </h2>
+      </div>
+      <div className="govuk-!-display-none-print">
+        <BackButton />
+
+        <div width="two-thirds">
+          <h2 className="lbh-heading-h2 display-inline govuk-!-margin-right-6">
+            WO {workOrder.reference.toString().padStart(8, '0')}
+          </h2>
+        </div>
+        <div width="one-third">
+          {workOrder.isHigherPriority() ? (
+            <h2 className=" text-dark-red lbh-heading-h2 display-inline govuk-!-margin-right-6">
+              {workOrder.priority.split(' ').slice(-1)}
+            </h2>
+          ) : (
+            <h2 className="lbh-heading-h2 display-inline govuk-!-margin-right-6">
+              {workOrder.priority.split(' ').slice(-1)}
+            </h2>
+          )}
+        </div>
+
+        <p className="lbh-body-m">{workOrder.description}</p>
+
+        <WorkOrderHeader
+          propertyReference={property.propertyReference}
+          workOrder={workOrder}
+          address={property.address}
+          locationAlerts={locationAlerts}
+          personAlerts={personAlerts}
+          subTypeDescription={property.hierarchyType.subTypeDescription}
+          tenure={tenure}
+          hasLinkToProperty={true}
+          canRaiseRepair={property.canRaiseRepair}
+          schedulerSessionId={schedulerSessionId}
+        />
+      </div>
+    </>
+  )
+}
+
+WorkOrderDetails.propTypes = {
+  property: PropTypes.object.isRequired,
+  workOrder: PropTypes.instanceOf(WorkOrder).isRequired,
+  locationAlerts: PropTypes.array.isRequired,
+  personAlerts: PropTypes.array.isRequired,
+  tenure: PropTypes.object.isRequired,
+}
+
+export default WorkOrderDetails
---
+++ src/components/WorkOrder/OperativeWorkOrderView.js
@@ -0,0 +1,150 @@
+import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react'
+import OperativeWorkOrderDetails from './OperativeWorkOrderDetails'
+import Spinner from '../Spinner/Spinner'
+import ErrorMessage from '../Errors/ErrorMessage/ErrorMessage'
+import { frontEndApiRequest } from '../../utils/frontEndApiClient/requests'
+import { getOrCreateSchedulerSessionId } from '../../utils/frontEndApiClient/users/schedulerSession'
+import Tabs from '../Tabs'
+import { WorkOrder } from '../../models/workOrder'
+import { sortObjectsByDateKey } from '../../utils/date'
+
+const OperativeWorkOrderView = ({ workOrderReference }) => {
+  const [property, setProperty] = useState({})
+  const [workOrder, setWorkOrder] = useState({})
+  const [locationAlerts, setLocationAlerts] = useState([])
+  const [tasksAndSors, setTasksAndSors] = useState([])
+  const [personAlerts, setPersonAlerts] = useState([])
+  const [tenure, setTenure] = useState({})
+  const [schedulerSessionId, setSchedulerSessionId] = useState()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+  const tabsList = [
+    'Tasks and SORs',
+    'Notes',
+    'Pending variation',
+    'Work orders history',
+  ]
+
+  const { NEXT_PUBLIC_STATIC_IMAGES_BUCKET_URL } = process.env
+
+  const printClickHandler = (e) => {
+    e.preventDefault()
+
+    if (document.getElementById('rear-image')) {
+      window.print()
+    } else {
+      const workOrderRearImage = document.createElement('img')
+      workOrderRearImage.src = `${NEXT_PUBLIC_STATIC_IMAGES_BUCKET_URL}/work-order-rear.png`
+      workOrderRearImage.id = 'rear-image'
+
+      workOrderRearImage.addEventListener('load', () => window.print())
+
+      document
+        .getElementById('rear-image-container')
+        .appendChild(workOrderRearImage)
+    }
+  }
+
+  const getWorkOrderView = async (workOrderReference) => {
+    setError(null)
+
+    try {
+      const workOrder = await frontEndApiRequest({
+        method: 'get',
+        path: `/api/workOrders/${workOrderReference}`,
+      })
+      const propertyObject = await frontEndApiRequest({
+        method: 'get',
+        path: `/api/properties/${workOrder.propertyReference}`,
+      })
+
+      const tasksAndSors = await frontEndApiRequest({
+        method: 'get',
+        path: `/api/workOrders/${workOrderReference}/tasks`,
+      })
+
+      setTasksAndSors(
+        sortObjectsByDateKey(tasksAndSors, ['dateAdded'], 'dateAdded')
+      )
+
+      // Call getOrCreateSchedulerSessionId if it is a DRS work order
+      if (workOrder.externalAppointmentManagementUrl) {
+        const schedulerSessionId = await getOrCreateSchedulerSessionId()
+        setSchedulerSessionId(schedulerSessionId)
+      }
+
+      setWorkOrder(new WorkOrder(workOrder))
+      setProperty(propertyObject.property)
+      setLocationAlerts(propertyObject.alerts.locationAlert)
+      setPersonAlerts(propertyObject.alerts.personAlert)
+      if (propertyObject.tenure) setTenure(propertyObject.tenure)
+    } catch (e) {
+      setWorkOrder(null)
+      setProperty(null)
+      console.error('An error has occured:', e.response)
+
+      if (e.response?.status === 404) {
+        setError(
+          `Could not find a work order with reference ${workOrderReference}`
+        )
+      } else {
+        setError(
+          `Oops an error occurred with error status: ${e.response?.status} with message: ${e.response?.data?.message}`
+        )
+      }
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    setLoading(true)
+
+    getWorkOrderView(workOrderReference)
+  }, [])
+
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          {property &&
+            property.address &&
+            property.hierarchyType &&
+            tenure &&
+            locationAlerts &&
+            personAlerts &&
+            workOrder && (
+              <>
+                <OperativeWorkOrderDetails
+                  property={property}
+                  workOrder={workOrder}
+                  tenure={tenure}
+                  locationAlerts={locationAlerts}
+                  personAlerts={personAlerts}
+                  schedulerSessionId={schedulerSessionId}
+                  tasksAndSors={tasksAndSors}
+                  printClickHandler={printClickHandler}
+                />
+                <Tabs
+                  tabsList={tabsList}
+                  propertyReference={property.propertyReference}
+                  workOrderReference={workOrderReference}
+                  tasksAndSors={tasksAndSors}
+                />
+              </>
+            )}
+          {error && <ErrorMessage label={error} />}
+        </>
+      )}
+    </>
+  )
+}
+
+OperativeWorkOrderView.propTypes = {
+  workOrderReference: PropTypes.string.isRequired,
+}
+
+export default OperativeWorkOrderView
