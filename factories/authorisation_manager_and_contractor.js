9d10b400ff74cb7218168f1cc0f8532e9f59782e
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-09-14 21:14:40 +0100

    Avoid unnecessary exports

--- factories/authorisation_manager_and_contractor.js
+++ factories/authorisation_manager_and_contractor.js
@@ -1,18 +1,16 @@
 import { Factory } from 'fishery'
 
-export const authorisationManagerAndContractorUserFactory = Factory.define(
-  () => ({
-    name: 'A DLO Supervisor',
-    email: 'a.dlo_supervisor@hackney.gov.uk',
-    roles: ['dlo_supervisor'],
-    hasRole: true,
-    hasAgentPermissions: false,
-    hasContractorPermissions: true,
-    hasContractManagerPermissions: false,
-    hasAuthorisationManagerPermissions: true,
-    hasOperativePermissions: false,
-    hasAnyPermissions: true,
-  })
-)
+const authorisationManagerAndContractorUserFactory = Factory.define(() => ({
+  name: 'A DLO Supervisor',
+  email: 'a.dlo_supervisor@hackney.gov.uk',
+  roles: ['dlo_supervisor'],
+  hasRole: true,
+  hasAgentPermissions: false,
+  hasContractorPermissions: true,
+  hasContractManagerPermissions: false,
+  hasAuthorisationManagerPermissions: true,
+  hasOperativePermissions: false,
+  hasAnyPermissions: true,
+}))
 
 export const authorisationManagerAndContractor = authorisationManagerAndContractorUserFactory.build()
