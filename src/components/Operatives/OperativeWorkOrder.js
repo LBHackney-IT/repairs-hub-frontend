3df017e50cfd99e1c52d901f7e8d988ef5c772fe
Evangelos Giataganas <e.giataganas@gmail.com>
2021-10-15 16:40:20 +0100

    Extract the logic to a separate component OperativeWorkOrder

---
+++ src/components/Operatives/OperativeWorkOrder.js
@@ -0,0 +1,49 @@
+import OperativeWorkOrderDetails from './OperativeWorkOrderDetails'
+import OperativeTasksAndSorsTable from '../WorkOrder/TasksAndSors/OperativeTasksAndSorsTable'
+import WarningInfoBox from '../Template/WarningInfoBox'
+import Link from 'next/link'
+import { sortArrayByDate } from '../../utils/helpers/array'
+
+const OperativeWorkOrder = ({
+  workOrderReference,
+  property,
+  workOrder,
+  personAlerts,
+  locationAlerts,
+  tasksAndSors,
+}) => {
+  return (
+    <>
+      <OperativeWorkOrderDetails
+        property={property}
+        workOrder={workOrder}
+        personAlerts={personAlerts}
+        locationAlerts={locationAlerts}
+        tasksAndSors={tasksAndSors}
+      />
+
+      <OperativeTasksAndSorsTable
+        tasksAndSors={sortArrayByDate(tasksAndSors, 'dateAdded')}
+        tabName={'Tasks and SORs'}
+      />
+
+      <WarningInfoBox
+        header="Need to make a change?"
+        text="Any changes to the work order must be made on paper."
+      />
+
+      <Link href={`/work-orders/${workOrderReference}/close`}>
+        <a
+          role="button"
+          draggable="false"
+          class="govuk-button lbh-button"
+          data-module="govuk-button"
+        >
+          Confirm
+        </a>
+      </Link>
+    </>
+  )
+}
+
+export default OperativeWorkOrder
