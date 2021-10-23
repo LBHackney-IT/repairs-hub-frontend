9d2d4bae0aa5d311fd7505287bf6caaf2330951d
Sia <anastasia.pankina@unboxedconsulting.com>
2021-10-23 20:53:43 +0100

    Completed Operative can add new SOR functionality

--- src/components/Operatives/NewTaskForm.js
+++ src/components/Operatives/NewTaskForm.js
@@ -1,18 +1,22 @@
 import { useForm } from 'react-hook-form'
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { getSorCodes } from 'src/utils/frontEndApiClient/scheduleOfRates/codes'
 import { frontEndApiRequest } from '../../utils/frontEndApiClient/requests'
-import Button from '../Form/Button'
+import { PrimarySubmitButton } from '../Form'
 import BackButton from '../Layout/BackButton'
 import RateScheduleItem from '../WorkElement/RateScheduleItem'
 import Spinner from '../Spinner'
 import ErrorMessage from '../Errors/ErrorMessage'
+import { buildWorkOrderUpdate } from '../../utils/hact/workOrderStatusUpdate/updateWorkOrder'
 
 const NewTaskForm = ({ workOrderReference }) => {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState()
   const [sorCodes, setSorCodes] = useState([])
-  const { register } = useForm()
+  const [existingTasks, setExistingTasks] = useState({})
+  const { register, handleSubmit, errors } = useForm()
+  const router = useRouter()
 
   const getNewTaskForm = async (reference) => {
     setError(null)
@@ -28,8 +32,50 @@ const NewTaskForm = ({ workOrderReference }) => {
         workOrder.contractorReference
       )
       setSorCodes(sorCodes)
+
+      const tasksAndSors = await frontEndApiRequest({
+        method: 'get',
+        path: `/api/workOrders/${workOrderReference}/tasks`,
+      })
+
+      setExistingTasks(tasksAndSors)
     } catch (e) {
       setSorCodes([])
+      setError(
+        `Oops an error occurred with error status: ${e.response?.status} with message: ${e.response?.data?.message}`
+      )
+    }
+
+    setLoading(false)
+  }
+
+  const onFormSubmit = async (e) => {
+    setLoading(true)
+
+    const newTask = [
+      {
+        ...e.rateScheduleItems.operative,
+        description: e.rateScheduleItems.operative.code.split(' - ')[1],
+      },
+    ]
+
+    const workOrderUpdateFormData = buildWorkOrderUpdate(
+      existingTasks,
+      newTask,
+      workOrderReference
+    )
+
+    try {
+      await frontEndApiRequest({
+        method: 'post',
+        path: `/api/jobStatusUpdate`,
+        requestData: workOrderUpdateFormData,
+      })
+
+      router.push(`/work-orders/${workOrderReference}`)
+    } catch (e) {
+      console.error(e)
+
       setError(
         `Oops an error occurred with error status: ${e.response?.status} with message: ${e.response?.data?.message}`
       )
@@ -57,15 +103,25 @@ const NewTaskForm = ({ workOrderReference }) => {
               <h1 className="lbh-heading-h2 govuk-!-margin-bottom-4">
                 New SOR
               </h1>
-              <RateScheduleItem sorCodesList={sorCodes} register={register} />
-              <div className="button-pair">
-                <Button
-                  width="one-third"
-                  label="Confirm"
-                  type="submit"
-                  isSecondary={false}
+              <form
+                role="form"
+                id="repair-request-form"
+                onSubmit={handleSubmit(onFormSubmit)}
+              >
+                <RateScheduleItem
+                  sorCodesList={sorCodes.map(
+                    (sorCode) => `${sorCode.code} - ${sorCode.shortDescription}`
+                  )}
+                  register={register}
+                  showRemoveRateScheduleItem={false}
+                  removeRateScheduleIte={false}
+                  errors={errors}
+                  index={'operative'}
                 />
-              </div>
+                <div className="button-pair">
+                  <PrimarySubmitButton label="Confirm" />
+                </div>
+              </form>
             </>
           )}
           {error && <ErrorMessage label={error} />}
