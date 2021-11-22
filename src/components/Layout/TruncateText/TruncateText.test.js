d31020118ca58adfd96acaff9d92c94e3e937557
Sia <anastasia.pankina@unboxedconsulting.com>
2021-11-22 15:35:12 +0000

    Add snapshot to TruncateText component

---
+++ src/components/Layout/TruncateText/TruncateText.test.js
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import TruncateText from './TruncateText'
+
+describe('TruncateText component', () => {
+  const props = {
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  }
+
+  it('should render truncate text, with truncate line of 3', () => {
+    const { asFragment } = render(
+      <TruncateText text={props.text} numberOfLines="3" />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should render truncate text, with truncate line of 5', () => {
+    const { asFragment } = render(
+      <TruncateText text={props.text} numberOfLines="5" />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
