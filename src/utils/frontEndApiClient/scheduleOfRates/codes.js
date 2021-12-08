5b304ec6537efaf6069a3051bd609530d7eac7c5
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-12-08 20:43:01 +0000

    Remove get sors codes helper

--- src/utils/frontEndApiClient/scheduleOfRates/codes.js
+++ src/utils/frontEndApiClient/scheduleOfRates/codes.js
@@ -1,21 +1,5 @@
 import axios from 'axios'
 
-export const getSorCodes = async (
-  tradeCode,
-  propertyReference,
-  contractorReference
-) => {
-  const { data } = await axios.get('/api/schedule-of-rates/codes', {
-    params: {
-      tradeCode: tradeCode,
-      propertyReference: propertyReference,
-      contractorReference: contractorReference,
-    },
-  })
-
-  return data
-}
-
 export const getSorCode = async (
   code,
   propertyReference,
--- src/utils/frontEndApiClient/scheduleOfRates/codes.test.js
+++ src/utils/frontEndApiClient/scheduleOfRates/codes.test.js
@@ -1,31 +1,7 @@
-import { getSorCodes, getSorCode } from './codes'
+import { getSorCode } from './codes'
 import mockAxios from 'axios'
 
 jest.mock('axios')
-
-describe('getSorCodes`', () => {
-  it('calls the Next JS API', async () => {
-    const responseData = { data: 'test' }
-
-    mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({
-        data: responseData,
-      })
-    )
-
-    const response = await getSorCodes('PL', '00001234', 'H01')
-
-    expect(response).toEqual(responseData)
-    expect(mockAxios.get).toHaveBeenCalledTimes(1)
-    expect(mockAxios.get).toHaveBeenCalledWith('/api/schedule-of-rates/codes', {
-      params: {
-        contractorReference: 'H01',
-        propertyReference: '00001234',
-        tradeCode: 'PL',
-      },
-    })
-  })
-})
 
 describe('getSorCode`', () => {
   it('calls the Next JS API with a contractor reference', async () => {
