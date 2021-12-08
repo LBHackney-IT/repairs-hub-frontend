2148d0d38450c2fbfd22b92239e424b73bc5b1c9
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-12-08 19:26:07 +0000

    Add unit test coverage for API helper
    Also:
     - Remove redundant tests

--- src/utils/frontEndApiClient/requests.test.js
+++ src/utils/frontEndApiClient/requests.test.js
@@ -3,69 +3,49 @@ import axios from 'axios'
 jest.mock('axios', () => jest.fn())
 
 describe('frontEndApiRequest`', () => {
-  // get filters request
-  it('calls the Next JS API to show filters', async () => {
+  it('calls axios with the expected parameters and returns data', async () => {
     const responseData = { data: 'test' }
+
     axios.mockResolvedValueOnce({
       data: responseData,
     })
+
     const response = await frontEndApiRequest({
       method: 'get',
-      path: '/api/filter/WorkOrder',
+      path: '/api/some_endpoint',
       params: { myParam: 1 },
       requestData: { some: 'data' },
     })
+
     expect(response).toEqual(responseData)
     expect(axios).toHaveBeenCalledTimes(1)
     expect(axios).toHaveBeenCalledWith({
       method: 'get',
-      url: '/api/filter/WorkOrder',
+      url: '/api/some_endpoint',
       params: { myParam: 1 },
       data: { some: 'data' },
     })
   })
 
-  // get current user request
-  it('calls the Next JS API to get current user', async () => {
+  it('does not send a data parameter if requestData is not supplied', async () => {
     const responseData = { data: 'test' }
-    axios.mockResolvedValueOnce({
-      data: responseData,
-    })
-    const response = await frontEndApiRequest({
-      method: 'get',
-      path: '/api/hub-user',
-      params: { myParam: 1 },
-      requestData: { some: 'data' },
-    })
-    expect(response).toEqual(responseData)
-    expect(axios).toHaveBeenCalledTimes(1)
-    expect(axios).toHaveBeenCalledWith({
-      method: 'get',
-      url: '/api/hub-user',
-      params: { myParam: 1 },
-      data: { some: 'data' },
-    })
-  })
 
-  // get work order request
-  it('calls the Next JS API to get work order', async () => {
-    const responseData = { data: 'test' }
     axios.mockResolvedValueOnce({
       data: responseData,
     })
+
     const response = await frontEndApiRequest({
       method: 'get',
-      path: '/api/workOrders/10000012',
+      path: '/api/some_endpoint',
       params: { myParam: 1 },
-      requestData: { some: 'data' },
     })
+
     expect(response).toEqual(responseData)
     expect(axios).toHaveBeenCalledTimes(1)
     expect(axios).toHaveBeenCalledWith({
       method: 'get',
-      url: '/api/workOrders/10000012',
+      url: '/api/some_endpoint',
       params: { myParam: 1 },
-      data: { some: 'data' },
     })
   })
 })
