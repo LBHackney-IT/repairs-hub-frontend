b0cfc458bb4e79f4728d4831be2228d64043614f
Sia <anastasia.pankina@unboxedconsulting.com>
2021-12-09 17:20:26 +0000

    Add function to check for overtime and unit tests

--- src/utils/helpers/completionDateTimes.test.js
+++ src/utils/helpers/completionDateTimes.test.js
@@ -1,6 +1,8 @@
 import {
   calculateCompletionDateTime,
+  isCurrentTimeOperativeOvertime,
   isCurrentTimeOutOfHours,
+  isOperativeOverTime,
 } from './completionDateTimes'
 import MockDate from 'mockdate'
 import {
@@ -462,6 +464,165 @@ describe('isCurrentTimeOutOfHours', () => {
       it('returns false', () => {
         expect(isCurrentTimeOutOfHours()).toEqual(false)
       })
+    })
+  })
+})
+
+describe('isCurrentTimeOperativeOvertime', () => {
+  beforeEach(() => {
+    mockBankHolidays.mockReturnValue({
+      'england-and-wales': {
+        events: [],
+      },
+    })
+  })
+  describe('when it is a working day', () => {
+    const dateTime = new Date('Thursday 9 December 2021')
+
+    afterEach(() => {
+      MockDate.reset()
+    })
+
+    describe('and the time is before 8am', () => {
+      beforeEach(() => {
+        MockDate.set(dateTime.setHours(7, 59, 0))
+      })
+
+      it('returns true', () => {
+        expect(isCurrentTimeOperativeOvertime()).toEqual(true)
+      })
+    })
+
+    describe('and the time is between 8am and 4pm', () => {
+      beforeEach(() => {
+        MockDate.set(dateTime.setHours(12, 0, 0))
+      })
+
+      it('returns false', () => {
+        expect(isCurrentTimeOperativeOvertime()).toEqual(false)
+      })
+    })
+
+    describe('and the time is after 4pm', () => {
+      beforeEach(() => {
+        MockDate.set(dateTime.setHours(16, 0, 1))
+      })
+
+      it('returns true', () => {
+        expect(isCurrentTimeOperativeOvertime()).toEqual(true)
+      })
+    })
+  })
+
+  describe('when it is a Saturday', () => {
+    const dateTime = new Date('Saturday 11 December 2021 12:00:00')
+    beforeEach(() => {
+      MockDate.set(dateTime)
+    })
+    it('returns true', () => {
+      expect(isCurrentTimeOperativeOvertime()).toEqual(true)
+    })
+  })
+
+  describe('when it is a Sunday', () => {
+    const dateTime = new Date('Sunday 12 December 2021 12:00:00')
+    beforeEach(() => {
+      MockDate.set(dateTime)
+    })
+    it('returns true', () => {
+      expect(isCurrentTimeOperativeOvertime()).toEqual(true)
+    })
+  })
+
+  describe('when it is a bank holiday', () => {
+    const dateTime = new Date('Monday 13 December 2021 12:00:00')
+    beforeEach(() => {
+      MockDate.set(dateTime)
+
+      mockBankHolidays.mockReturnValue({
+        'england-and-wales': {
+          division: 'england-and-wales',
+          events: [
+            {
+              title: 'Fake bank holiday',
+              date: '2021-12-13',
+              notes: '',
+              bunting: true,
+            },
+          ],
+        },
+      })
+    })
+    it('returns true', () => {
+      expect(isCurrentTimeOperativeOvertime()).toEqual(true)
+    })
+  })
+})
+
+describe('isOperativeOverTime', () => {
+  beforeEach(() => {
+    mockBankHolidays.mockReturnValue({
+      'england-and-wales': {
+        events: [],
+      },
+    })
+  })
+  describe('when it is a working day', () => {
+    const dateTime = new Date('Thursday 9 December 2021')
+
+    describe('and the time is before 8am', () => {
+      it('returns true', () => {
+        expect(isOperativeOverTime(dateTime.setHours(7, 59, 0))).toEqual(true)
+      })
+    })
+
+    describe('and the time is between 8am and 4pm', () => {
+      it('returns false', () => {
+        expect(isOperativeOverTime(dateTime.setHours(12, 0, 0))).toEqual(false)
+      })
+    })
+
+    describe('and the time is after 4pm', () => {
+      it('returns true', () => {
+        expect(isOperativeOverTime(dateTime.setHours(16, 0, 1))).toEqual(true)
+      })
+    })
+  })
+
+  describe('when it is a Saturday', () => {
+    const dateTime = new Date('Saturday 11 December 2021 12:00:00')
+
+    it('returns true', () => {
+      expect(isOperativeOverTime(dateTime)).toEqual(true)
+    })
+  })
+
+  describe('when it is a Sunday', () => {
+    const dateTime = new Date('Sunday 12 December 2021 12:00:00')
+    it('returns true', () => {
+      expect(isOperativeOverTime(dateTime)).toEqual(true)
+    })
+  })
+
+  describe('when it is a bank holiday', () => {
+    const dateTime = new Date('Monday 13 December 2021 12:00:00')
+    beforeEach(() => {
+      mockBankHolidays.mockReturnValue({
+        'england-and-wales': {
+          division: 'england-and-wales',
+          events: [
+            {
+              title: 'Fake bank holiday',
+              date: '2021-12-13',
+              notes: '',
+              bunting: true,
+            },
+          ],
+        },
+      })
+    })
+    it('returns true', () => {
+      expect(isOperativeOverTime(dateTime)).toEqual(true)
     })
   })
 })
