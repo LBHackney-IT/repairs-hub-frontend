7088ac2a3360d011cafe7163bfc8d96938024303
Sia <anastasia.pankina@unboxedconsulting.com>
2022-01-17 14:02:36 +0000

    Add Planned to priority list

--- src/utils/helpers/priorities.js
+++ src/utils/helpers/priorities.js
@@ -2,10 +2,16 @@ export const IMMEDIATE_PRIORITY_CODE = 1
 export const EMERGENCY_PRIORITY_CODE = 2
 export const URGENT_PRIORITY_CODE = 3
 export const NORMAL_PRIORITY_CODE = 4
+export const PLANNED_PRIORITY_CODE = 9
 
 export const HIGH_PRIORITY_CODES = [
   IMMEDIATE_PRIORITY_CODE,
   EMERGENCY_PRIORITY_CODE,
+]
+export const LOW_PRIORITY_CODES = [
+  URGENT_PRIORITY_CODE,
+  NORMAL_PRIORITY_CODE,
+  PLANNED_PRIORITY_CODE,
 ]
 
 // These are codes which should result in using the RH
