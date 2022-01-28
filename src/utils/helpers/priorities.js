31f1884aa2b403ea06ea65c1ecfcee5775b354ec
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2022-01-28 14:18:28 +0000

    Invert logic for detecting low priority
    We want to get away from storing priority information on the
    frontend, especially as more priorities are being added to the API. But
    for the purposes of detecting if an order is affected by low
    priority holidays we still need some kind of categorisation of
    priorities. This commit achieves that by making a list of the high
    priorities. This should require less maintenance as we expect those to
    be more stable than a list of low priorities.
    
    A better longer term solution would be to receive the
    priority type from the backend, or even for it to calculate its own
    completion times, but we're not there currently.

--- src/utils/helpers/priorities.js
+++ src/utils/helpers/priorities.js
@@ -3,6 +3,12 @@ export const EMERGENCY_PRIORITY_CODE = 2
 export const URGENT_PRIORITY_CODE = 3
 export const NORMAL_PRIORITY_CODE = 4
 
-export const LOW_PRIORITY_CODES = [URGENT_PRIORITY_CODE, NORMAL_PRIORITY_CODE]
+export const HIGH_PRIORITY_CODES = [
+  IMMEDIATE_PRIORITY_CODE,
+  EMERGENCY_PRIORITY_CODE,
+]
 
-export const priorityCodesRequiringAppointments = LOW_PRIORITY_CODES
+export const priorityCodesRequiringAppointments = [
+  URGENT_PRIORITY_CODE,
+  NORMAL_PRIORITY_CODE,
+]
