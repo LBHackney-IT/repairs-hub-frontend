3793effa6a768a1ca5697ec54a7b03d16e68fcd9
Sia <anastasia.pankina@unboxedconsulting.com>
2022-02-03 18:23:42 +0000

    Address comments

--- src/utils/hact/workOrderSchedule/raiseWorkOrderForm.test.js
+++ src/utils/hact/workOrderSchedule/raiseWorkOrderForm.test.js
@@ -1,5 +1,8 @@
 import MockDate from 'mockdate'
-import { URGENT_PRIORITY_CODE } from '../../helpers/priorities'
+import {
+  URGENT_PRIORITY_CODE,
+  PLANNED_PRIORITY_CODE,
+} from '../../helpers/priorities'
 import { buildScheduleWorkOrderFormData } from './raiseWorkOrderForm'
 
 const mockBankHolidays = jest.fn()
@@ -45,7 +48,30 @@ describe('buildScheduleWorkOrderFormData', () => {
     callerName: 'John Smith',
     contactNumber: '07788777888',
   }
-
+  const workOrderDataForPlannedPriority = {
+    rateScheduleItems: [
+      {
+        code: 'DES5R005 - Normal call outs',
+        description: 'Normal call outs',
+        quantity: '3.25',
+      },
+    ],
+    trade: 'Plumbing',
+    tradeCode: 'PL',
+    contractor: 'Purdy Contracts (P) Ltd - PCL',
+    contractorRef: 'PCL',
+    quantity: '1',
+    priorityDescription: '9[P] PLANNED MAINT',
+    priorityCode: PLANNED_PRIORITY_CODE,
+    daysToComplete: 365,
+    hoursToComplete: 365 * 24,
+    descriptionOfWork: 'This is a planned priority test description',
+    propertyReference: '00001234',
+    shortAddress: '12 Pitcairn House',
+    postalCode: 'E9 6PT',
+    callerName: 'John Smith',
+    contactNumber: '07788777888',
+  }
   it('builds the ScheduleWorkOrder form data to post to the Repairs API', async () => {
     MockDate.set(new Date('Thu Jan 14 2021 18:16:20Z'))
 
@@ -160,6 +186,97 @@ describe('buildScheduleWorkOrderFormData', () => {
     }
 
     const response = buildScheduleWorkOrderFormData(workOrderData)
+    expect(response).toEqual(scheduleWorkOrderFormData)
+  })
+
+  it('builds the ScheduleWorkOrder form data to post to the Repairs API with Planned Priority', async () => {
+    MockDate.set(new Date('Thu Jan 14 2021 18:16:20Z'))
+
+    const scheduleWorkOrderFormData = {
+      reference: [
+        {
+          id: 'aa757643-cf89-4247-a42c-8a035182feqd',
+        },
+      ],
+      descriptionOfWork: 'This is a planned priority test description',
+      priority: {
+        priorityCode: PLANNED_PRIORITY_CODE,
+        priorityDescription: '9[P] PLANNED MAINT',
+        requiredCompletionDateTime: new Date('Friday Jan 14 2022 18:16:20Z'),
+        numberOfDays: 365,
+      },
+      workClass: {
+        workClassCode: 0,
+      },
+      workElement: [
+        {
+          rateScheduleItem: [
+            {
+              customCode: 'DES5R005',
+              customName: 'Normal call outs',
+              quantity: { amount: [3.25] },
+            },
+          ],
+          trade: [
+            {
+              code: 'SP',
+              customCode: 'PL',
+              customName: 'Plumbing',
+            },
+          ],
+        },
+      ],
+      site: {
+        property: [
+          {
+            propertyReference: '00001234',
+            address: {
+              addressLine: ['12 Pitcairn House'],
+              postalCode: 'E9 6PT',
+            },
+            reference: [
+              {
+                id: '00001234',
+              },
+            ],
+          },
+        ],
+      },
+      instructedBy: {
+        name: 'Hackney Housing',
+      },
+      assignedToPrimary: {
+        name: 'Purdy Contracts (P) Ltd',
+        organization: {
+          reference: [
+            {
+              id: 'PCL',
+            },
+          ],
+        },
+      },
+      customer: {
+        name: 'John Smith',
+        person: {
+          name: {
+            full: 'John Smith',
+          },
+          communication: [
+            {
+              channel: {
+                medium: '20',
+                code: '60',
+              },
+              value: '07788777888',
+            },
+          ],
+        },
+      },
+    }
+
+    const response = buildScheduleWorkOrderFormData(
+      workOrderDataForPlannedPriority
+    )
     expect(response).toEqual(scheduleWorkOrderFormData)
   })
 })
