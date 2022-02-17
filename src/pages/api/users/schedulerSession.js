90944cbe238b5f33b7178f8fda9b956d046bb20b
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2022-02-17 18:50:45 +0000

    Capture Sentry errors for DRS API route

--- src/pages/api/users/schedulerSession.js
+++ src/pages/api/users/schedulerSession.js
@@ -1,6 +1,7 @@
 import * as HttpStatus from 'http-status-codes'
 import { createDRSSession } from '@/utils/scheduling/drs/webServices/sessions'
 import { authoriseServiceAPIRequest } from '@/utils/serviceApiClient'
+import { captureException } from '@sentry/nextjs'
 
 export default authoriseServiceAPIRequest(async (req, res) => {
   try {
@@ -10,6 +11,8 @@ export default authoriseServiceAPIRequest(async (req, res) => {
 
     res.status(HttpStatus.OK).json(data)
   } catch (e) {
+    captureException(e)
+
     console.error(`Cannot create DRS Web Services session with error: ${e}`)
 
     const errorToThrow = new Error(e)
