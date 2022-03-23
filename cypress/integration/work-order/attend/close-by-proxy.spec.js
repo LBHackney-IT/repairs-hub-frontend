4cbdea320be978f31829687e28db0e765b06249b
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2022-03-23 09:56:53 +0000

    Increase a request timeout

--- cypress/integration/work-order/attend/close-by-proxy.spec.js
+++ cypress/integration/work-order/attend/close-by-proxy.spec.js
@@ -207,7 +207,7 @@ describe('Closing a work order on behalf of an operative', () => {
 
     cy.get('[type="submit"]').contains('Confirm and close').click()
 
-    cy.wait('@apiCheck', { requestTimeout: 6000 })
+    cy.wait('@apiCheck', { requestTimeout: 7000 })
       .its('request.body')
       .should('deep.equal', {
         workOrderReference: {
