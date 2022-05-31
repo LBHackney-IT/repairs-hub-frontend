4255beb4d8fe3d6754a512dbc3087a3f735e2c8e
Sia <anastasia.pankina@unboxedconsulting.com>
2022-05-31 16:59:02 +0100

    Update PageAnnouncement component

--- src/components/Template/PageAnnouncement.js
+++ src/components/Template/PageAnnouncement.js
@@ -4,10 +4,7 @@ export const PageAnnouncement = (props) => (
   <section className="text-align-left lbh-page-announcement">
     <h3 className="lbh-page-announcement__title">{props.title}</h3>
     <div className="lbh-page-announcement__content">
-      <p>Reference number</p>
-      <strong className="govuk-!-font-size-24">
-        {props.workOrderReference}
-      </strong>
+      <p>Reference number {props.workOrderReference}</p>
     </div>
   </section>
 )
