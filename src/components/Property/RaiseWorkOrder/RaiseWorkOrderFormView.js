a3711b378b342f0542f19f13a40b07ad931b38f4
ange47rm <ange47rm@hotmail.com>
2022-08-03 18:05:28 +0100

    Removed duplicate getCurrentSORCodes() function and console.log from RaiseWorkOrderFormView.js

--- src/components/Property/RaiseWorkOrder/RaiseWorkOrderFormView.js
+++ src/components/Property/RaiseWorkOrder/RaiseWorkOrderFormView.js
@@ -237,17 +237,6 @@ const RaiseWorkOrderFormView = ({ propertyReference }) => {
       )
     )
   }
-  console.log(formState)
-
-  const getCurrentSORCodes = () => {
-    if (formState != null && formState.rateScheduleItems == null) {
-      formState.rateScheduleItems = []
-    }
-
-    return [
-      ...formState?.rateScheduleItems.map((rsi) => rsi.code.split(' - ')[0]),
-    ]
-  }
 
   const getCurrentSORCodes = () => {
     if (formState != null && formState.rateScheduleItems == null) {
