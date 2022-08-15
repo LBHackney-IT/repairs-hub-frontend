384b87710643df0c3ad1ece1805fd660e8e3bc5b
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-08-15 13:59:56 +0100

    Reused unchanged variables

--- src/components/WorkOrder/PrintJobTicketDetails.test.js
+++ src/components/WorkOrder/PrintJobTicketDetails.test.js
@@ -2,7 +2,26 @@ import { render } from '@testing-library/react'
 import PrintJobTicketDetails from './PrintJobTicketDetails'
 import { WorkOrder } from '../../models/workOrder'
 
+const tasksAndSors = [
+  {
+    code: 'code',
+    description: 'description',
+    quantity: 2,
+    standardMinuteValue: 2,
+  },
+]
 
+const property = {
+  address: {
+    addressLine: '14 Pitcairn House St Thomass Square',
+    streetSuffix: 'Dwelling',
+    postalCode: 'E9 6PT',
+  },
+  tmoName: 'tmoName',
+}
+
+const locationAlerts = []
+const personAlerts = []
 
 describe('PrintJobTicketDetails component', () => {
   it('Renders the PrintJobTicketDetails component', () => {
@@ -23,27 +42,6 @@ describe('PrintJobTicketDetails component', () => {
       description: 'description',
       plannerComments: 'plannerComments',
     }
-    
-    const property = {
-      address: {
-        addressLine: '14 Pitcairn House St Thomass Square',
-        streetSuffix: 'Dwelling',
-        postalCode: 'E9 6PT',
-      },
-      tmoName: 'tmoName',
-    }
-    
-    const tasksAndSors = [
-      {
-        code: 'code',
-        description: 'description',
-        quantity: 2,
-        standardMinuteValue: 2,
-      },
-    ]
-    
-    const locationAlerts = []
-    const personAlerts = []
 
     const { asFragment } = render(
       <PrintJobTicketDetails
@@ -71,28 +69,7 @@ describe('PrintJobTicketDetails component', () => {
       description: 'description',
       plannerComments: 'plannerComments',
     }
-    
-    const property = {
-      address: {
-        addressLine: '14 Pitcairn House St Thomass Square',
-        streetSuffix: 'Dwelling',
-        postalCode: 'E9 6PT',
-      },
-      tmoName: 'tmoName',
-    }
-    
-    const tasksAndSors = [
-      {
-        code: 'code',
-        description: 'description',
-        quantity: 2,
-        standardMinuteValue: 2,
-      },
-    ]
-    
-    const locationAlerts = []
-    const personAlerts = []
-    
+
     const { asFragment } = render(
       <PrintJobTicketDetails
         workOrder={new WorkOrder(workOrderData)}
