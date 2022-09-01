c7d549eb94a31c04abe3cfb1b5cc3319f241d00a
ange47rm <ange47rm@hotmail.com>
2022-09-01 12:40:30 +0100

    Modified error box in AddMultipleSORs as per Figma wireframe

--- src/components/Property/RaiseWorkOrder/AddMultipleSORs.js
+++ src/components/Property/RaiseWorkOrder/AddMultipleSORs.js
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form'
 import { SorCodeValidator } from '@/utils/helpers/SorCodeValidator'
 import { useState } from 'react'
 import Spinner from '@/components/Spinner'
+import WarningInfoBox from '../../Template/WarningInfoBox'
 
 const AddMultipleSORs = ({
   currentSorCodes,
@@ -56,26 +57,13 @@ const AddMultipleSORs = ({
     setPerformingValidation(false)
   }
 
-  const renderErrorSummary = (errors) => (
-    <div
-      class="govuk-error-summary optional-extra-class lbh-error-summary"
-      aria-labelledby="error-summary-title"
-      role="alert"
-      tabindex="-1"
-      data-module="govuk-error-summary"
-    >
-      <h2 class="govuk-error-summary__title" id="error-summary-title">
-        Invalid SOR codes entered
-      </h2>
-      <div class="govuk-error-summary__body">
-        <ul class="govuk-list govuk-error-summary__list">
-          {errors.map((error, i) => (
-            <li key={i}>{`${error.code}: ${error.message}`}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
+  const renderErrorSummary = (errors) => {
+    const errorList = errors.map(error => `"${error.code}"`);
+
+    return (
+    <WarningInfoBox header={`Invalid SOR code(s) entered: ${errorList.join(" ")}`} text=""/>
+    )
+  }
 
   return (
     <>
@@ -94,8 +82,6 @@ const AddMultipleSORs = ({
         Add multiple SOR codes
       </h1>
 
-      {validationErrors.length > 0 && renderErrorSummary(validationErrors)}
-
       <form
         role="form"
         id="adding-multiple-sors-form"
@@ -111,6 +97,8 @@ const AddMultipleSORs = ({
           rows={6}
           placeholder="04500910&#10;49PLMAT2&#10;RTR03016"
         />
+
+        {validationErrors.length > 0 && renderErrorSummary(validationErrors)}
 
         <PrimarySubmitButton
           label="Submit"
