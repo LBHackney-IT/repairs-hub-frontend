e987fc1f67ba9516c66f86460aeb1399230830ab
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2023-01-12 13:08:13 +0000

    Add bulk-close form (#734)
    * Add bulk-close form
    
    * Add SuccessMessage component
    
    * Lintfix
    
    * Add backoffice ControlledRadio component
    
    * Update propTypes for radio component
    
    * Fix bad ref

---
+++ src/components/BackOffice/Components/ControlledRadio/index.js
@@ -0,0 +1,103 @@
+import cx from 'classnames'
+import PropTypes from 'prop-types'
+
+import ErrorMessage from '../../../Errors/ErrorMessage'
+
+const defaultOptions = ['Yes', 'No']
+
+const ControlledRadio = ({
+  label,
+  labelSize = 'm',
+  hint,
+  name,
+  selectedOption,
+  options = defaultOptions,
+  register,
+  error,
+  children,
+  required,
+  isRadiosInline = false,
+  ...otherProps
+}) => (
+  <div
+    className={cx('govuk-form-group lbh-form-group', {
+      'govuk-form-group--error': error,
+    })}
+  >
+    <label className={`govuk-label govuk-label--${labelSize}`} htmlFor={name}>
+      {label} {required && <span className="govuk-required">*</span>}
+    </label>
+    {hint && (
+      <span id={`${name}-hint`} className="govuk-hint lbh-hint">
+        {hint}
+      </span>
+    )}
+    {children}
+    {error && <ErrorMessage label={error.message} />}
+    <div
+      className={cx('govuk-radios lbh-radios', {
+        'govuk-radios--inline': isRadiosInline,
+      })}
+    >
+      {options.map((option) => {
+        const { value, text, defaultChecked, hint } =
+          typeof option === 'string'
+            ? { value: option, text: option, defaultChecked: false }
+            : option
+        return (
+          <div className="govuk-radios__item" key={text}>
+            <input
+              className={cx('govuk-radios__input', {
+                'govuk-input--error': error,
+              })}
+              id={`${name}_${value}`}
+              name={name}
+              type="radio"
+              value={value}
+              ref={register}
+              data-testid={name}
+              aria-describedby={
+                hint &&
+                `${name}-${value.replace(/\s+/g, '-').toLowerCase()}-hint`
+              }
+              defaultChecked={defaultChecked}
+              // should only apply when parameter passed
+              checked={selectedOption !== null && selectedOption === value}
+              {...otherProps}
+            />
+
+            <label
+              className="govuk-label lbh-label govuk-radios__label"
+              htmlFor={`${name}_${value}`}
+            >
+              {text}
+            </label>
+
+            {hint && (
+              <span
+                id={`${name}-${value.replace(/\s+/g, '-').toLowerCase()}-hint`}
+                className="govuk-hint govuk-radios__hint"
+              >
+                {hint}
+              </span>
+            )}
+          </div>
+        )
+      })}
+    </div>
+  </div>
+)
+
+ControlledRadio.propTypes = {
+  label: PropTypes.string.isRequired,
+  labelSize: PropTypes.oneOf(['s', 'm', 'l', 'xl']),
+  name: PropTypes.string.isRequired,
+  selectedOption: PropTypes.string,
+  register: PropTypes.func,
+  options: PropTypes.array,
+  hint: PropTypes.string,
+  children: PropTypes.node,
+  error: PropTypes.shape({ message: PropTypes.string.isRequired }),
+}
+
+export default ControlledRadio
