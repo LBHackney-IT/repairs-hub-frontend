4316aae92ad2b293cb8bd6e113524c0720a1f6dc
ange47rm <ange47rm@hotmail.com>
2023-02-13 10:23:13 +0000

    Prettier autofix

--- src/components/BackOffice/Components/NewSORCode/index.test.js
+++ src/components/BackOffice/Components/NewSORCode/index.test.js
@@ -4,12 +4,22 @@ import NewSORCode from '.'
 
 describe('NewSORCode component', () => {
   it('should render the component', async () => {
-    const sorCode = new SorCode(1, 'ASD123YO', 25.50, 47, 'Short Description', 'Long Description')
-    const { asFragment } = render(<NewSORCode 
-      key={sorCode.id}
-      sorCode={sorCode}
-      handleRemoveSORCode={jest.fn()}
-      handleSORCodeFieldChange={jest.fn()}/>)
+    const sorCode = new SorCode(
+      1,
+      'ASD123YO',
+      25.5,
+      47,
+      'Short Description',
+      'Long Description'
+    )
+    const { asFragment } = render(
+      <NewSORCode
+        key={sorCode.id}
+        sorCode={sorCode}
+        handleRemoveSORCode={jest.fn()}
+        handleSORCodeFieldChange={jest.fn()}
+      />
+    )
 
     expect(asFragment()).toMatchSnapshot()
   })
