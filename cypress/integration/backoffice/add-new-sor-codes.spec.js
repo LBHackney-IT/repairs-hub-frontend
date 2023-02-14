fe18dc4b4f419f607c44172489b279cb0907dc45
ange47rm <ange47rm@hotmail.com>
2023-02-14 15:31:27 +0000

    Prettier autofix

--- cypress/integration/backoffice/add-new-sor-codes.spec.js
+++ cypress/integration/backoffice/add-new-sor-codes.spec.js
@@ -3,70 +3,66 @@
 import 'cypress-audit/commands'
 
 function contractorsRequest() {
-    cy.intercept(
-        { method: 'GET', path: '/api/contractors?getAllContractors=true', },
-        { fixture: 'contractors/contractors.json' }
-    ).as('contractorsRequest')
+  cy.intercept(
+    { method: 'GET', path: '/api/contractors?getAllContractors=true' },
+    { fixture: 'contractors/contractors.json' }
+  ).as('contractorsRequest')
 }
 
 function tradesRequest() {
-    cy.intercept(
-        { method: 'GET', path: '/api/schedule-of-rates/trades?getAllTrades=true', },
-        { fixture: 'scheduleOfRates/trades.json' }
-    ).as('tradesRequest')
+  cy.intercept(
+    { method: 'GET', path: '/api/schedule-of-rates/trades?getAllTrades=true' },
+    { fixture: 'scheduleOfRates/trades.json' }
+  ).as('tradesRequest')
 }
 
 function contractsRequest() {
-    cy.intercept(
-        { method: 'GET', path: '/api/backoffice/contracts?contractorReference=FPS' },
-        { fixture: 'contractors/contracts.json' }
-    ).as('contractsRequest')
+  cy.intercept(
+    {
+      method: 'GET',
+      path: '/api/backoffice/contracts?contractorReference=FPS',
+    },
+    { fixture: 'contractors/contracts.json' }
+  ).as('contractsRequest')
 }
 
 describe('Add New Sor Codes - when user unauthorized', () => {
-    it("Shows access denied when user doesn't have correct permissions", () => {
-        cy.loginWithOperativeRole()
+  it("Shows access denied when user doesn't have correct permissions", () => {
+    cy.loginWithOperativeRole()
 
-        cy.visit('/backoffice/add-sor-codes')
-        cy.contains('Access denied')
-    })
+    cy.visit('/backoffice/add-sor-codes')
+    cy.contains('Access denied')
+  })
 })
 
 describe('Add New SOR Codes', () => {
-    beforeEach(() => {
-        cy.loginWithDataAdminRole()
-        contractorsRequest()
-        tradesRequest()
-        contractsRequest()
+  beforeEach(() => {
+    cy.loginWithDataAdminRole()
+    contractorsRequest()
+    tradesRequest()
+    contractsRequest()
 
-        cy.visit('/backoffice/add-sor-codes')
-    })
+    cy.visit('/backoffice/add-sor-codes')
+  })
 
-    it('a GET request is triggered on page loads to retrieve contractors', () => {
+  it('a GET request is triggered on page loads to retrieve contractors', () => {
+    cy.wait('@contractorsRequest')
+      .its('request.method')
+      .should('deep.equal', 'GET')
+  })
 
-        cy.wait('@contractorsRequest')
-            .its('request.method')
-            .should('deep.equal', 'GET')
-    })
+  it('a GET request is triggered on page loads to retrieve trades', () => {
+    cy.wait('@tradesRequest').its('request.method').should('deep.equal', 'GET')
+  })
 
-    it('a GET request is triggered on page loads to retrieve trades', () => {
+  it('a GET request is triggered on page loads to retrieve available contracts when a contractor is selected', () => {
+    cy.wait('@contractorsRequest')
+    cy.wait('@tradesRequest')
 
-        cy.wait('@tradesRequest')
-            .its('request.method')
-            .should('deep.equal', 'GET')
-    })
+    cy.get('[data-testid="contractor"]').type('Fairway Property Services')
 
-    it('a GET request is triggered on page loads to retrieve available contracts when a contractor is selected', () => {
-
-        cy.wait('@contractorsRequest')
-        cy.wait('@tradesRequest')
-        
-        cy.get('[data-testid="contractor"]').type("Fairway Property Services")
-
-        cy.wait('@contractsRequest')
-            .its('request.method')
-            .should('deep.equal', 'GET')
-    })
-
-
+    cy.wait('@contractsRequest')
+      .its('request.method')
+      .should('deep.equal', 'GET')
+  })
 })
