220ab0b805d9946245741b70b09c03a6df8e423a
ange47rm <ange47rm@hotmail.com>
2023-02-16 12:54:00 +0000

    Another lint fix

--- src/components/BackOffice/CloseWorkOrders/index.js
+++ src/components/BackOffice/CloseWorkOrders/index.js
@@ -72,7 +72,7 @@ const CloseWorkOrders = () => {
 
     const strippedWorkOrderReferences = formatWorkOrderReferences(
       workOrderReferences
-      )
+    )
     const invalidWorkOrderReferences = getInvalidWorkOrderReferences(
       strippedWorkOrderReferences
     )
