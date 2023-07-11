0286175987ca5f740885670bf23059e76970e954
Dave Dean <david.dean@hackney.gov.uk>
2023-07-11 20:23:44 +0100

    lint

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = {
   presets: ['next/babel'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-private-methods'
+    '@babel/plugin-transform-private-methods',
   ],
 }
