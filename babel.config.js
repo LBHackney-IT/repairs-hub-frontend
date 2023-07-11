a943cbab0eec89f757f76a1b9598350c5309c29b
Dave Dean <david.dean@hackney.gov.uk>
2023-07-11 19:32:09 +0100

    plz

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   presets: ['next/babel'],
-  plugins: 
-    [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-transform-private-methods'
-    ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-private-methods'
+  ],
 }
