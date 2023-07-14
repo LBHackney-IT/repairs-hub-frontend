543b88aa8857a40570af0676abc7247a919684a4
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-07-14 12:34:02 +0100

    lintfix

--- cypress/integration/property/boilerHouseFlag.spec.js
+++ cypress/integration/property/boilerHouseFlag.spec.js
@@ -98,7 +98,10 @@ describe('Boiler house flag', () => {
 
     // assert boiler house link is visible
     cy.get('[data-testid="boiler-house-details-link"]').contains(
-      'Booster Pump 1-93 Pitcairn House  St Thomass Square', { timeout: 4000 }
+      'Booster Pump 1-93 Pitcairn House  St Thomass Square',
+      {
+        timeout: 4000,
+      }
     )
     cy.get('[data-testid="boiler-house-details-link"]')
       .should('have.attr', 'href')
