bfe4f1dc451e9701d815598b8c4d9e39b27aaee8
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-07-17 12:35:00 +0100

    Remove mock response from api

--- src/pages/api/properties/asset-id/[id]/index.js
+++ src/pages/api/properties/asset-id/[id]/index.js
@@ -1,31 +1,13 @@
 import * as HttpStatus from 'http-status-codes'
-import { authoriseServiceAPIRequest } from '@/utils/serviceApiClient'
-
-const mockResponse = {
-  propertyReference: '00023404',
-  address: {
-    shortAddress: '16 Pitcairn House St Thomass Square',
-    postalCode: 'E9 6PT',
-    addressLine: '16 Pitcairn House St Thomass Square',
-    streetSuffix: null,
-  },
-  hierarchyType: {
-    levelCode: null,
-    subTypeCode: null,
-    subTypeDescription: 'Dwelling',
-  },
-  tmoName: 'London Borough of Hackney',
-  canRaiseRepair: false,
-}
+import { authoriseServiceAPIRequest, serviceAPIRequest } from '@/utils/serviceApiClient'
 
 export default authoriseServiceAPIRequest(async (req, res) => {
   req.query = { path: ['properties', 'asset-id', req.query.id] }
 
   try {
-    // const data = await serviceAPIRequest(req, res)
+    const data = await serviceAPIRequest(req, res)
 
-    // res.status(HttpStatus.OK).json(data )
-    res.status(HttpStatus.OK).json(mockResponse)
+    res.status(HttpStatus.OK).json(data)
   } catch (error) {
     const errorToThrow = new Error(error)
 
