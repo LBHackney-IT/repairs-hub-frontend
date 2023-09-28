26c89acebfa204ca075ea2465fb8690b607ccc15
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2023-09-28 17:58:23 +0100

    Fetch contact details from new endpoint (#774) (#775)
    * Fetch contact details from contact details endpoint directly
    
    * rename data to propertyResponse
    
    * typo
    
    * Lintfix
    
    * redact contact information if user doesnt have permissions
    
    * lintfix
    
    * attempt 2
    
    * fix tests - attempt
    
    * update E2E tests
    
    * new attempt
    
    * promise.all
    
    * add intercepts for contact details
    
    * lintfix
    
    * cleanup
    
    * remove spaces

--- cypress/integration/work-order/create/appointment-form.spec.js
+++ cypress/integration/work-order/create/appointment-form.spec.js
@@ -19,6 +19,14 @@ describe('Schedule appointment form', () => {
     cy.intercept(
       {
         method: 'GET',
+        path: '/api/contact-details/4552c539-2e00-8533-078d-9cc59d9115da',
+      },
+      { fixture: 'contactDetails/contactDetails.json' }
+    ).as('contactDetailsRequest')
+
+    cy.intercept(
+      {
+        method: 'GET',
         path: '/api/contractors?propertyReference=00012345&tradeCode=PL',
       },
       { fixture: 'contractors/contractors.json' }
