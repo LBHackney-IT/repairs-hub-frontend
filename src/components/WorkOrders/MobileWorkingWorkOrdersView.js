45489ec0cb2cb69027f542d62fa9a9b539ce25db
Dave Dean <david.dean@hackney.gov.uk>
2023-10-09 17:16:42 +0100

    lint

--- src/components/WorkOrders/MobileWorkingWorkOrdersView.js
+++ src/components/WorkOrders/MobileWorkingWorkOrdersView.js
@@ -95,12 +95,14 @@ const MobileWorkingWorkOrdersView = () => {
               <ol className="lbh-list mobile-working-work-order-list">
                 {renderWorkOrderListItems(
                   currentUser.isOneJobAtATime
-                    ? inProgressWorkOrders.sort((a, b => {
-                      const dateA = new Date(a.assignedStartTime);
-                      const dateB = new Date(b.assignedStartTime);
-                      // Get first job by assigned start time
-                      return dateA - dateB;
-                    }).slice(0, 1))
+                    ? inProgressWorkOrders
+                        .sort((a, b) => {
+                          const dateA = new Date(a.assignedStartTime)
+                          const dateB = new Date(b.assignedStartTime)
+                          // Get first job by assigned start time
+                          return dateA - dateB
+                        })
+                        .slice(0, 1)
                     : inProgressWorkOrders
                 )}
                 {renderWorkOrderListItems(visitedWorkOrders)}
