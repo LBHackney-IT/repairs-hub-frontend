7314dc20a59faa0c9518363a834bd233762165a2
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-11-06 16:04:16 +0000

    Cleanup test

--- src/pages/api/contact-details/index.test.js
+++ src/pages/api/contact-details/index.test.js
@@ -17,19 +17,6 @@ const {
 const CONTACT_DETAILS_API_URL = 'https://test.com/development'
 process.env.CONTACT_DETAILS_API_URL = CONTACT_DETAILS_API_URL
 
-// const TENURE_ID = '250af5a91f04316aac4a7a737e98874'
-
-// const CONTACT_DETAILS = [
-//   {
-//     fullName: 'FirstName LastName',
-//     phoneNumbers: ['0123456789', '0123456789'],
-//   },
-//   {
-//     fullName: 'FirstName LastName',
-//     phoneNumbers: ['0123456789', '0123456789'],
-//   },
-// ]
-
 describe('/api/contact-details', () => {
   beforeEach(() => {
     axios.create = jest.fn(() => axios)
@@ -37,13 +24,9 @@ describe('/api/contact-details', () => {
     axios.mockImplementationOnce(() =>
       Promise.resolve({
         status: 200,
-        // data: CONTACT_DETAILS,
       })
     )
   })
-
-  // DELETE request forwards request to correct endpoint
-  //
 
   const signedCookie = jsonwebtoken.sign(
     {
@@ -78,12 +61,10 @@ describe('/api/contact-details', () => {
 
     await contactDetailsEndpoint(req, res)
 
-    // CONTACT_DETAILS_API_URL
     expect(axios).toHaveBeenCalledTimes(1)
     expect(axios).toHaveBeenCalledWith({
       method: 'DELETE',
       headers,
-      // url: `${CONTACT_DETAILS_API_URL}/api/v2/contactDetails/${contactId}/person/${personId}`,
       url: `${CONTACT_DETAILS_API_URL}/api/v1/contactDetails`,
       params: {
         id: contactId,
@@ -111,7 +92,6 @@ describe('/api/contact-details', () => {
 
     await contactDetailsEndpoint(req, res)
 
-    // CONTACT_DETAILS_API_URL
     expect(axios).toHaveBeenCalledTimes(1)
     expect(axios).toHaveBeenCalledWith({
       method: 'PATCH',
@@ -123,143 +103,3 @@ describe('/api/contact-details', () => {
     })
   })
 })
-
-// describe('GET /api/contact-details/[id] contact information redaction', () => {
-//   beforeEach(() => {
-//     axios.create = jest.fn(() => axios)
-
-//     axios.mockImplementationOnce(() =>
-//       Promise.resolve({
-//         status: 200,
-//         data: CONTACT_DETAILS,
-//       })
-//     )
-//   })
-
-//   describe('when the cookie is for an agent', () => {
-//     const signedCookie = jsonwebtoken.sign(
-//       {
-//         name: 'name',
-//         email: 'name@example.com',
-//         groups: [AGENTS_GOOGLE_GROUPNAME],
-//       },
-//       HACKNEY_JWT_SECRET
-//     )
-
-//     const headers = {
-//       'Content-Type': 'application/json',
-//       'x-api-key': REPAIRS_SERVICE_API_KEY,
-//       'x-hackney-user': signedCookie,
-//       Authorization: signedCookie,
-//     }
-
-//     test('the response can include full contact information', async () => {
-//       const req = createRequest({
-//         method: 'get',
-//         headers: { Cookie: `${GSSO_TOKEN_NAME}=${signedCookie};` },
-//         query: {
-//           id: TENURE_ID,
-//         },
-//         params: {},
-//       })
-
-//       const res = createResponse()
-
-//       await contactDetailsEndpoint(req, res)
-
-//       expect(axios).toHaveBeenCalledTimes(1)
-//       expect(axios).toHaveBeenCalledWith({
-//         method: 'get',
-//         headers,
-//         url: `${REPAIRS_SERVICE_API_URL}/contact-details/${TENURE_ID}`,
-//         params: {},
-//         paramsSerializer,
-//         data: {},
-//       })
-
-//       const parsedData = JSON.parse(res._getData())
-
-//       expect(parsedData).toEqual(CONTACT_DETAILS)
-//     })
-//   })
-
-//   describe('when the cookie is for an authorisation manager', () => {
-//     const signedCookie = jsonwebtoken.sign(
-//       {
-//         name: 'name',
-//         email: 'name@example.com',
-//         groups: [AUTHORISATION_MANAGERS_GOOGLE_GROUPNAME],
-//       },
-//       HACKNEY_JWT_SECRET
-//     )
-
-//     const headers = {
-//       'Content-Type': 'application/json',
-//       'x-api-key': REPAIRS_SERVICE_API_KEY,
-//       'x-hackney-user': signedCookie,
-//       Authorization: signedCookie,
-//     }
-
-//     test('the response can include full contact information', async () => {
-//       const req = createRequest({
-//         method: 'get',
-//         headers: { Cookie: `${GSSO_TOKEN_NAME}=${signedCookie};` },
-//         query: {
-//           id: TENURE_ID,
-//         },
-//         params: {},
-//       })
-
-//       const res = createResponse()
-
-//       await contactDetailsEndpoint(req, res)
-
-//       expect(axios).toHaveBeenCalledTimes(1)
-//       expect(axios).toHaveBeenCalledWith({
-//         method: 'get',
-//         headers,
-//         url: `${REPAIRS_SERVICE_API_URL}/contact-details/${TENURE_ID}`,
-//         params: {},
-//         paramsSerializer,
-//         data: {},
-//       })
-
-//       const parsedData = JSON.parse(res._getData())
-
-//       expect(parsedData).toEqual(CONTACT_DETAILS)
-//     })
-//   })
-
-//   describe('when the cookie is for a contractor', () => {
-//     const signedCookie = jsonwebtoken.sign(
-//       {
-//         name: 'name',
-//         email: 'name@example.com',
-//         groups: [CONTRACTORS_GOOGLE_GROUPNAME],
-//       },
-//       HACKNEY_JWT_SECRET
-//     )
-
-//     test('response contact information is redacted', async () => {
-//       const req = createRequest({
-//         method: 'get',
-//         headers: { Cookie: `${GSSO_TOKEN_NAME}=${signedCookie};` },
-//         query: {
-//           id: TENURE_ID,
-//         },
-//         params: {},
-//       })
-
-//       const res = createResponse()
-
-//       await contactDetailsEndpoint(req, res)
-
-//       // ["REMOVED"] is returned before API call if user doesn't have valid permissions
-//       expect(axios).toHaveBeenCalledTimes(0)
-
-//       const parsedData = JSON.parse(res._getData())
-
-//       expect(parsedData).toEqual(['REMOVED'])
-//     })
-//   })
-// })
