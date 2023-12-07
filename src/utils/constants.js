7b2c9141b89ea82c92da65b2cd09853765d37b42
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2023-12-07 12:09:12 +0000

    Add missed config for enabling fpm as multitrade contractor (#792)
    * Enable FPM  as multi-trade contractor
    
    * lintfix
    
    * Rename constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,19 @@ export const MULTITRADE_TRADE_CODE = 'MU'
 // Feature toggle keys
 export const MULTITRADE_SOR_INCREMENTAL_SEARCH_ENABLED_KEY =
   'MultiTradeSORIncrementalSearch'
+
+// If a contractor doesnt have any 'MU' sorCodes, the contractor will
+// not be listed when multi-trade has been selected as the trade.
+// However, multi-trade is special, and will show sorCodes from any trade,
+// so a contractor should need to have 'MU' sorCodes.
+// To get around this, we must hardcode these contractors in the dropdown
+export const MULTITRADE_CONTRACTORS_WITHOUT_MU_SORCODES = [
+  {
+    contractorReference: 'PCL',
+    contractorName: 'Purdy Contracts (P) Ltd',
+  },
+  {
+    contractorReference: 'FPM',
+    contractorName: 'Foster Property Maintenance',
+  },
+]
