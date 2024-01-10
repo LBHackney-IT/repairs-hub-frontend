32da3d78ae3cad45221c0bc38ce78498503acb58
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-01-10 12:28:24 +0000

    Lintfix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,10 @@
-export const MULTITRADE_ENABLED_CONTRACTORS = ['PCL', 'AEP', 'HHL', 'FPM', 'HCW']
+export const MULTITRADE_ENABLED_CONTRACTORS = [
+  'PCL',
+  'AEP',
+  'HHL',
+  'FPM',
+  'HCW',
+]
 export const PURDY_CONTRACTOR_REFERENCE = 'PCL'
 export const MULTITRADE_TRADE_CODE = 'MU'
 
