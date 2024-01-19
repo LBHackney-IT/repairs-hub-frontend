63305e55a6d951b7350222113d025f9e1143e3b0
Adam <adam.tracy20@gmail.com>
2024-01-19 09:45:01 +0000

    Account for new param in unit test

--- src/utils/hact/workOrderComplete/closeWorkOrder.test.js
+++ src/utils/hact/workOrderComplete/closeWorkOrder.test.js
@@ -97,6 +97,7 @@ describe.only('buildDampAndMouldReportData', () => {
           // Act
           const result = buildDampAndMouldReportData(
             '',
+            '',
             previouslyReportedValue,
             value,
             ''
