a3570bb58d37dc5eee773c9fb666e6f323714054
Adam <adam.tracy20@gmail.com>
2024-01-19 12:54:48 +0000

    Added response headers for security

--- next.config.js
+++ next.config.js
@@ -1,11 +1,38 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 
+const cspHeader = `
+  frame-ancestors 'none';
+`
+
+async function headers() {
+  return [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value: cspHeader.replace(/\n/g, ''),
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'strict-origin-when-cross-origin',
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+      ],
+    },
+  ]
+}
+
 const moduleExports = {
   distDir: 'build/_next',
   target: 'server',
   experimental: {
     forceSwcTransforms: true,
   },
+  headers: () => headers(),
 }
 
 const { NODE_ENV, SENTRY_RELEASE } = process.env
