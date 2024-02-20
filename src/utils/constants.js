a5f4192f0cf9daf947e3bb08f677154b1b267b8a
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2024-02-20 13:59:13 +0000

    Remove deprecated FT for incremental search (#829)
    * delete
    
    * Undo
    
    * Cleaup tests
    
    * LF
    
    * update snapshots
    
    * upd sc
    
    * update other sc

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,10 +9,6 @@ export const MULTITRADE_ENABLED_CONTRACTORS = [
 export const PURDY_CONTRACTOR_REFERENCE = 'PCL'
 export const MULTITRADE_TRADE_CODE = 'MU'
 
-// Feature toggle keys
-export const MULTITRADE_SOR_INCREMENTAL_SEARCH_ENABLED_KEY =
-  'MultiTradeSORIncrementalSearch'
-
 // If a contractor doesnt have any 'MU' sorCodes, the contractor will
 // not be listed when multi-trade has been selected as the trade.
 // However, multi-trade is special, and will show sorCodes from any trade,
