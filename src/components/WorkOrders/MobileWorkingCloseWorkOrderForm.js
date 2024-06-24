0a23987eed81689987fc8b2b3648df9fae80edcb
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-06-24 15:38:52 +0100

    plis work

--- src/components/WorkOrders/MobileWorkingCloseWorkOrderForm.js
+++ src/components/WorkOrders/MobileWorkingCloseWorkOrderForm.js
@@ -5,6 +5,7 @@ import TextArea from '../Form/TextArea'
 import Radios from '../Form/Radios'
 import WarningInfoBox from '../Template/WarningInfoBox'
 import { CLOSURE_STATUS_OPTIONS } from '@/utils/statusCodes'
+import { PrimarySubmitButton } from '../Form'
 
 const MobileWorkingCloseWorkOrderForm = ({ onSubmit }) => {
   const { handleSubmit, register, errors } = useForm({
@@ -19,41 +20,41 @@ const MobileWorkingCloseWorkOrderForm = ({ onSubmit }) => {
         <h1 className="lbh-heading-h2">Close work order form</h1>
 
         <form role="form" onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <h2 className="lbh-heading-h4" style={{ marginBottom: '60px' }}>
-              Work order status
-            </h2>
+          <h2 className="lbh-heading-h4" style={{ marginBottom: '60px' }}>
+            Work order status
+          </h2>
 
-            <Radios
-              labelSize="s"
-              label="Select reason for closing"
-              name="reason"
-              options={CLOSURE_STATUS_OPTIONS.map((r) => {
-                return {
-                  text: r.text,
-                  value: r.value,
-                }
-              })}
-              register={register({
-                required: 'Please select a reason for closing the work order',
-              })}
-              error={errors && errors.reason}
+          <Radios
+            labelSize="s"
+            label="Select reason for closing"
+            name="reason"
+            options={CLOSURE_STATUS_OPTIONS.map((r) => {
+              return {
+                text: r.text,
+                value: r.value,
+              }
+            })}
+            register={register({
+              required: 'Please select a reason for closing the work order',
+            })}
+            error={errors && errors.reason}
+          />
+
+          <TextArea
+            name="notes"
+            label="Final report"
+            register={register}
+            error={errors && errors.notes}
+          />
+
+          <div className="govuk-!-margin-top-8">
+            <WarningInfoBox
+              header="Other changes?"
+              text="Any follow on and material change must be made on paper."
             />
+          </div>
 
-            <TextArea
-              name="notes"
-              label="Final report"
-              register={register}
-              error={errors && errors.notes}
-            />
-
-            <div className="govuk-!-margin-top-8">
-              <WarningInfoBox
-                header="Other changes?"
-                text="Any follow on and material change must be made on paper."
-              />
-            </div>
-          </div>{' '}
+          <PrimarySubmitButton label="Close work order" />
         </form>
       </div>
     </>
