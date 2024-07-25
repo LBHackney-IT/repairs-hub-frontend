da51d8ed4ba73c84475b1ed8668a1e6a7e1d1f4a
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-07-25 13:48:51 +0100

    forced

--- src/components/WorkOrder/Photos/PhotoViewList.js
+++ src/components/WorkOrder/Photos/PhotoViewList.js
@@ -6,45 +6,46 @@ const PhotoViewList = ({ photos, onSubmitSetDescription = null }) => {
   // in case of error
   if (photos === null) return null
 
+  const dateFormatOptions = { timeZone: 'UTC' }
+  // process.env.NODE_ENV === 'test' ? { timeZone: 'UTC' } : {}
+
   return (
     <>
       <ul className="lbh-body-s photoViewList-ul">
-        {photos.map((fileGroup) => {
-          return (
-            <li
-              key={fileGroup.id}
-              className="photoViewList-li"
-              data-testid={`fileGroup-${fileGroup.id}`}
-            >
-              <hr className="govuk-section-break govuk-section-break--visible photoViewList-hr" />
+        {photos.map((fileGroup) => (
+          <li
+            key={fileGroup.id}
+            className="photoViewList-li"
+            data-testid={`fileGroup-${fileGroup.id}`}
+          >
+            <hr className="govuk-section-break govuk-section-break--visible photoViewList-hr" />
 
-              <>
-                <div className="photoViewList-photoGroup">
-                  <h3>{fileGroup.groupLabel}</h3>
+            <>
+              <div className="photoViewList-photoGroup">
+                <h3>{fileGroup.groupLabel}</h3>
 
-                  <div className="govuk-!-margin-0">
-                    {format(
-                      new Date(fileGroup.timestamp),
-                      'dd LLLL yyyy, HH:mm',
-                      process.env.NODE_ENV === 'test' ? { timeZone: 'UTC' } : {}
-                    )}
-                  </div>
+                <div className="govuk-!-margin-0">
+                  {format(
+                    new Date(fileGroup.timestamp),
+                    'dd LLLL yyyy, HH:mm',
+                    dateFormatOptions
+                  )}
                 </div>
-                <p className="govuk-!-margin-top-1">{fileGroup.uploadedBy}</p>
+              </div>
+              <p className="govuk-!-margin-top-1">{fileGroup.uploadedBy}</p>
 
-                <PhotoListWithPreview fileUrls={fileGroup.files} />
-              </>
+              <PhotoListWithPreview fileUrls={fileGroup.files} />
+            </>
 
-              {onSubmitSetDescription !== null && (
-                <PhotoGroupDescriptionForm
-                  id={fileGroup.id}
-                  description={fileGroup.description}
-                  onSubmitSetDescription={onSubmitSetDescription}
-                />
-              )}
-            </li>
-          )
-        })}
+            {onSubmitSetDescription !== null && (
+              <PhotoGroupDescriptionForm
+                id={fileGroup.id}
+                description={fileGroup.description}
+                onSubmitSetDescription={onSubmitSetDescription}
+              />
+            )}
+          </li>
+        ))}
       </ul>
 
       <hr className="govuk-section-break govuk-section-break--visible photoViewList-hr" />
