8d3f55bcae6bf5332b7ca983e3ae1fb8e8651e8d
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-07-31 08:31:31 +0100

    Add notification banner

--- src/components/Layout/PhaseBanner/index.js
+++ src/components/Layout/PhaseBanner/index.js
@@ -1,23 +1,19 @@
 import PropTypes from 'prop-types'
-import Link from 'next/link'
 
 const PhaseBanner = ({ feedbackLink }) => {
   return (
-    <div className="govuk-phase-banner lbh-phase-banner lbh-container govuk-!-display-none-print">
-      <p className="govuk-phase-banner__content">
-        <strong className="govuk-tag govuk-phase-banner__content__tag lbh-tag">
-          Beta
-        </strong>
-        <span className="govuk-phase-banner__text">
-          This is a new service - your
-          <span> </span>
-          <Link href={feedbackLink}>
-            <a title="feedback">feedback</a>
-          </Link>{' '}
-          will help us to improve it.
-        </span>
-      </p>
-    </div>
+    <section className="lbh-announcement lbh-announcement--site govuk-!-display-none-print govuk-!-margin-0">
+      <div className="lbh-container">
+        {/* <h3 className="lbh-announcement__title">Site-wide announcement</h3> */}
+        <div className="lbh-announcement__content">
+          <p className="lbh-body-s">
+            You can now add photos to Repairs Hub!{' '}
+            <a href={feedbackLink}>Read more</a> about these changes and provide
+            your feedback.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
