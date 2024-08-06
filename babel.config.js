4768f2f608e74fccd3fe72dd011d5a2a5ed139fe
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-08-06 08:54:45 +0100

    add typescript

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,32 @@
+// module.exports = {
+//   presets: ['next/babel'],
+//   plugins: [
+//     '@babel/plugin-proposal-class-properties',
+//     '@babel/plugin-transform-private-methods',
+//   ],
+// }
+
+
 module.exports = {
-  presets: ['next/babel'],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-private-methods',
+  "presets": ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+  "plugins": [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        "regenerator": true
+      }
+    ]
   ],
-}
+  "env": {
+    "test": {
+      "presets": [
+        [
+          "@babel/preset-env",
+          {
+            "targets": "current node"
+          }
+        ]
+      ]
+    }
+  }
+}#
