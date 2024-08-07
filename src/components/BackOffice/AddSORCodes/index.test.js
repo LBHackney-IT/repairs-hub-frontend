3da9bf32930d549a5ccaba637080f0502893d5c6
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-08-07 12:12:12 +0100

    try fix test

--- src/components/BackOffice/AddSORCodes/index.test.js
+++ src/components/BackOffice/AddSORCodes/index.test.js
@@ -1,15 +1,12 @@
-import { render, act } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import AddSORCodes from '.'
+
+jest.mock('axios')
 
 describe('AddSORCodes component', () => {
   it('should render the component', async () => {
-    let component
+    const { asFragment } = render(<AddSORCodes />)
 
-    await act(async () => {
-      component = render(<AddSORCodes />)
-    })
-
-    const { asFragment } = component
     expect(asFragment()).toMatchSnapshot()
   })
 })
