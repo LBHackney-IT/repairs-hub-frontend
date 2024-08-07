aa24f7b663ddc0f089e06d3ca3d24f5e52ee897d
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-08-07 08:19:00 +0100

    add single ts file

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,25 +1,9 @@
 import qs from 'qs'
 
-export const absoluteUrl = (req, setLocalhost) => {
-  let protocol = 'https:'
-  let host = req
-    ? req.headers['x-forwarded-host'] || req.headers['host']
-    : window.location.host
-  if (host.indexOf('localhost') > -1) {
-    if (setLocalhost) host = setLocalhost
-    protocol = 'http:'
-  }
-  return {
-    protocol: protocol,
-    host: host,
-    origin: protocol + '//' + host,
-  }
-}
-
 export const getProtocol = () => {
   return process.env.NODE_ENV === 'production' ? 'https' : 'http'
 }
 
-export const paramsSerializer = (params) => {
+export const paramsSerializer = (params: any) => {
   return qs.stringify(params, { arrayFormat: 'repeat' })
 }
