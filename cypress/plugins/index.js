5231d3d78779f9e144a36a1fde13172a3aa6845d
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-08-07 13:02:08 +0100

    test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -40,6 +40,12 @@ module.exports = (on, config) => {
 
   on('before:browser:launch', (browser = {}, launchOptions) => {
     prepareAudit(launchOptions)
+
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      launchOptions.args.push('--disable-gpu');
+    }
+    
+    return launchOptions;
   })
 
   on('task', {
