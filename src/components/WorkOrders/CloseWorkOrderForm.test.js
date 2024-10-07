0b6341d33de404a8ef16b6baf65b48bcdde01861
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-10-07 10:55:35 +0100

    Update snapshots

--- src/components/WorkOrders/CloseWorkOrderForm.test.js
+++ src/components/WorkOrders/CloseWorkOrderForm.test.js
@@ -4,6 +4,102 @@ import UserContext from '../UserContext'
 import CloseWorkOrderForm from './CloseWorkOrderForm'
 
 describe('CloseWorkOrderForm component', () => {
+  process.env.NEXT_PUBLIC_FOLLOW_ON_FUNCTIONALITY_ENABLED = 'false'
+
+  const operatives = [
+    {
+      id: 1,
+      name: 'Operative A',
+    },
+    {
+      id: 2,
+      name: 'Operative B',
+    },
+  ]
+
+  const props = {
+    reference: 10000012,
+    onSubmit: jest.fn(),
+    notes: 'this is a note',
+    completionTime: '14:30',
+    completionDate: new Date('2021-01-12T16:24:26.632Z'),
+    startTime: '14:30',
+    startDate: new Date('2021-01-12T16:24:26.632Z'),
+    operatives: operatives,
+    availableOperatives: operatives,
+    reason: 'No Access',
+    selectedPercentagesToShowOnEdit: [],
+    totalSMVs: 76,
+  }
+
+  it('should render properly', () => {
+    const { asFragment } = render(
+      <UserContext.Provider
+        value={{
+          user: agent,
+        }}
+      >
+        <CloseWorkOrderForm
+          reference={props.reference}
+          onSubmit={props.onSubmit}
+          notes={props.notes}
+          completionTime={props.completionTime}
+          completionDate={props.completionDate}
+          startTime={props.startTime}
+          startDate={props.startDate}
+          reason={props.reason}
+          operativeAssignmentMandatory={true}
+          assignedOperativesToWorkOrder={props.operatives}
+          availableOperatives={props.availableOperatives}
+          selectedPercentagesToShowOnEdit={
+            props.selectedPercentagesToShowOnEdit
+          }
+          totalSMV={props.totalSMVs}
+          jobIsSplitByOperative={false}
+          paymentType={'Overtime'}
+          existingStartTime={false}
+        />
+      </UserContext.Provider>
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should not render startDate or startTime', () => {
+    const { asFragment } = render(
+      <UserContext.Provider
+        value={{
+          user: agent,
+        }}
+      >
+        <CloseWorkOrderForm
+          reference={props.reference}
+          onSubmit={props.onSubmit}
+          notes={props.notes}
+          completionTime={props.completionTime}
+          completionDate={props.completionDate}
+          startTime={props.startTime}
+          startDate={props.startDate}
+          reason={props.reason}
+          operativeAssignmentMandatory={true}
+          assignedOperativesToWorkOrder={props.operatives}
+          availableOperatives={props.availableOperatives}
+          selectedPercentagesToShowOnEdit={
+            props.selectedPercentagesToShowOnEdit
+          }
+          totalSMV={props.totalSMVs}
+          jobIsSplitByOperative={false}
+          paymentType={'Overtime'}
+          existingStartTime={true}
+        />
+      </UserContext.Provider>
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
+
+describe('CloseWorkOrderForm component - when follow-on functionality is enabled', () => {
+  process.env.NEXT_PUBLIC_FOLLOW_ON_FUNCTIONALITY_ENABLED = 'true'
+
   const operatives = [
     {
       id: 1,
--- src/components/WorkOrders/MobileWorkingCloseWorkOrderForm.test.js
+++ src/components/WorkOrders/MobileWorkingCloseWorkOrderForm.test.js
@@ -2,6 +2,19 @@ import { render } from '@testing-library/react'
 import MobileWorkingCloseWorkOrderForm from './MobileWorkingCloseWorkOrderForm'
 
 describe('MobileWorkingCloseWorkOrderForm component', () => {
+  process.env.NEXT_PUBLIC_FOLLOW_ON_FUNCTIONALITY_ENABLED = 'false'
+
+  it('should render properly', () => {
+    const { asFragment } = render(
+      <MobileWorkingCloseWorkOrderForm onSubmit={jest.fn()} />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
+
+describe('MobileWorkingCloseWorkOrderForm component - when follow-on functionality is enabled', () => {
+  process.env.NEXT_PUBLIC_FOLLOW_ON_FUNCTIONALITY_ENABLED = 'true'
+
   it('should render properly', () => {
     const { asFragment } = render(
       <MobileWorkingCloseWorkOrderForm onSubmit={jest.fn()} />
