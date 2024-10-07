c12f356f4d1d8930e184022a12bde427cd756b67
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-10-07 14:24:52 +0100

    revert cy clock

--- cypress/e2e/work-order/attend/close.cy.js
+++ cypress/e2e/work-order/attend/close.cy.js
@@ -102,7 +102,7 @@ describe('Closing my own work order', () => {
 
   context('during normal working hours', () => {
     beforeEach(() => {
-      // cy.clock(new Date(now).setHours(12, 0, 0))
+      cy.clock(new Date(now).setHours(12, 0, 0))
     })
 
     it('shows a validation error when no reason is selected', () => {
@@ -477,7 +477,7 @@ describe('Closing my own work order', () => {
 
   context('when outside working hours (overtime could apply)', () => {
     beforeEach(() => {
-      // cy.clock(new Date(now).setHours(16, 0, 1))
+      cy.clock(new Date(now).setHours(16, 0, 1))
     })
 
     context('and the overtime payment type is chosen', () => {
