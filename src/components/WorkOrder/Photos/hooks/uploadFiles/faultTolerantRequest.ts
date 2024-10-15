f4a6ddbeaf92f895f7132f15c088265bff2ef314
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <88662046+LBHCallumM@users.noreply.github.com>
2024-10-15 14:34:58 +0100

    Update src/components/WorkOrder/Photos/hooks/uploadFiles/faultTolerantRequest.ts
    Co-authored-by: Adam <74552077+adamtry@users.noreply.github.com>

--- src/components/WorkOrder/Photos/hooks/uploadFiles/faultTolerantRequest.ts
+++ src/components/WorkOrder/Photos/hooks/uploadFiles/faultTolerantRequest.ts
@@ -2,6 +2,10 @@ import retry from 'retry'
 
 const DEFAULT_NUMBER_OF_RETRIES = 3
 
+/**
+ * Returns a promise that tries to successfully resolve
+ * a specified amount of times with exponential backoff
+ */
 const faultTolerantRequest = async (
   request: () => Promise<void>,
   retries: number = DEFAULT_NUMBER_OF_RETRIES
