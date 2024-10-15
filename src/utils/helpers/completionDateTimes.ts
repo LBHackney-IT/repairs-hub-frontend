b0dff7cc6ab5658d321473931172ad2ae12438b6
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-10-15 09:03:18 +0100

    convert completionDateTImes to TS

--- src/utils/helpers/completionDateTimes.ts
+++ src/utils/helpers/completionDateTimes.ts
@@ -5,7 +5,7 @@ import { lowPriorityHolidays } from './lowPriorityHolidays'
 const WORKDAY_START_HOUR = 8
 const WORKDAY_END_HOUR = 18
 
-export const isBankHoliday = (date) => {
+const isBankHoliday = (date: Date) => {
   const formattedDate = format(date, 'yyyy-MM-dd')
   const englandWalesBankHolidays = bankHolidays['england-and-wales']['events']
 
@@ -31,13 +31,13 @@ export const isCurrentTimeOutOfHours = () => {
   return false
 }
 
-export const isNonWorkingDay = (date, lowPriority = false) =>
+const isNonWorkingDay = (date: Date, lowPriority: boolean = false) =>
   isWeekend(date) ||
   isBankHoliday(date) ||
   (lowPriority && isLowPriorityHoliday(date))
 
 // Sometimes Hackney have holidays which only apply to low priority orders.
-const isLowPriorityHoliday = (date) => {
+const isLowPriorityHoliday = (date: Date) => {
   const formattedDate = format(date, 'yyyy-MM-dd')
 
   return lowPriorityHolidays.some((holiday) => holiday === formattedDate)
@@ -47,7 +47,7 @@ export const isCurrentTimeOperativeOvertime = () => {
   return isOperativeOverTime(new Date())
 }
 
-export const isOperativeOverTime = (date) => {
+const isOperativeOverTime = (date: Date) => {
   const startOfOperativeDay = new Date(date).setHours(8, 0, 0)
   const endOfOperativeDay = new Date(date).setHours(16, 0, 0)
 
