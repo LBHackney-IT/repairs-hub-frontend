63f209f4deccefb4a4d812edbb325a478e791433
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-10-21 10:45:57 +0100

    add xray

--- src/lambda.js
+++ src/lambda.js
@@ -2,11 +2,19 @@ const server = require('restana')()
 const app = require('next')({ dev: false })
 const files = require('serve-static')
 const path = require('path')
+const AWSXRay = require('aws-xray-sdk')
+
+AWSXRay.enableAutomaticMode()
+
 const nextRequestHandler = app.getRequestHandler()
+
+server.use(AWSXRay.express.openSegment('NextJSApp')) // Open segment for tracing
 
 server.use(files(path.join(__dirname, 'build')))
 server.use(files(path.join(__dirname, 'public')))
 
 server.all('*', (req, res) => nextRequestHandler(req, res))
+
+server.use(AWSXRay.express.closeSegment()) // Close segment
 
 module.exports.handler = require('serverless-http')(server)
