0eac0d491a62b6676ca153c391765833895b3a8d
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <88662046+LBHCallumM@users.noreply.github.com>
2024-10-23 13:47:22 +0100

    Add OOH gas check to can be scheduled (#944)
    * Prevent OOH gas jobs from scheduling an appointment
    
    * refactor tests
    
    * Update tests
    
    * Update cypress tests

--- src/components/WorkOrder/AppointmentDetails.tsx
+++ src/components/WorkOrder/AppointmentDetails.tsx
@@ -14,7 +14,6 @@ import ScheduleAppointment from './ScheduleAppointment'
 const AppointmentDetails = ({
   workOrder,
   schedulerSessionId,
-
   resetSchedulerSessionId,
 }) => {
   const { user } = useContext(UserContext)
