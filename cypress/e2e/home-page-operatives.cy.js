cd1ccd5cc33fccac8054eefb26432baf7115e70a
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2024-11-11 15:50:15 +0000

    test tabs and work orders

--- cypress/e2e/home-page-operatives.cy.js
+++ cypress/e2e/home-page-operatives.cy.js
@@ -1,3 +1,7 @@
+/// <reference types="cypress" />
+
+import 'cypress-audit/commands'
+
 context('When an operative is logged in', () => {
   beforeEach(() => {
     cy.loginWithOperativeRole()
@@ -152,17 +156,45 @@ context('When an operative is logged in', () => {
     })
   })
 
-  context.only('When operative visits past work orders page', () => {
+  context.only('When operative clicks a tab', () => {
+    it('Goes to correct page', () => {
+      cy.intercept(
+        {
+          method: 'GET',
+          path: '/api/operatives/017233/workorders',
+        },
+        {
+          fixture: 'workOrders/workOrders11thNov.json',
+        }
+      ).as('operativesWorkOrders')
+      cy.intercept(
+        {
+          method: 'GET',
+          path: '/api/operatives/017233/workOrdersNew*',
+        },
+        {
+          fixture: 'pastWorkOrders/5thNovemberpastWorkOrders.json',
+        }
+      ).as('operativesWorkOrders')
+      cy.visit('/')
+      cy.get('.govuk-tabs_list-item-mobile-a-tag').should('have.length', 2)
+      cy.get('.govuk-tabs_list-item-mobile-a-tag').eq(1).click()
+      cy.url().should('include', 'oldjobs')
+      cy.get('.govuk-tabs_list-item-mobile-a-tag').eq(0).click()
+      cy.url().should('include', '/')
+    })
+  })
+
+  context('When operative visits past work orders page', () => {
     beforeEach(() => {
       cy.intercept(
         {
           method: 'GET',
-          path: `/api/operatives/017233/workOrdersNew**`,
-          // /api/operatives/017233/workOrdersNew?date=2024-11-10
+          path: `/api/operatives/017233/workOrdersNew*`,
+        },
+        {
+          fixture: 'pastWorkOrders/5thNovemberpastWorkOrders.json',
         }
-        // {
-        //   fixture: 'pastWorkOrders/5thNovemberpastWorkOrders.json',
-        // }
       ).as('pastOperativesWorkOrders')
       cy.visit('/oldjobs')
     })
@@ -171,60 +203,50 @@ context('When an operative is logged in', () => {
         cy.get('option').should('have.length', 5)
       })
     })
-    it('Displays past jobs for the chosen date', () => {
+    it('Displays past jobs with expected fields', () => {
+      cy.get('.operative-work-order-list-item').should('exist') // Ensure work orders are loaded
       cy.wait('@pastOperativesWorkOrders').then((interception) => {
         // Access the fixture data dynamically
         const fixtureData = interception.response.body
-        console.log(fixtureData)
-        // cy.get('.operative-work-order-list-item').should(
-        //   'have.length',
-        //   fixtureData.length
-        // )
-        //   cy.get('.lbh-list li').each((el, index) => {
-        //     const {
-        //       appointment: { start, end },
-        //       priority,
-        //       property,
-        //       propertyPostCode,
-        //       description,
-        //     } = fixtureData[index]
-        //     // Format start and end times for display as 'HH:mm – HH:mm'
-        //     const formattedTime = `${start} – ${end}`
-        //     // Verify that each element contains expected data from the fixture
-        //     cy.wrap(el).within(() => {
-        //       cy.contains(formattedTime)
-        //       cy.contains(priority.toLowerCase())
-        //       cy.contains(property)
-        //       cy.contains(propertyPostCode)
-        //       cy.contains(description)
-        // })
-        // })
+        cy.get('.operative-work-order-list-item').should(
+          'have.length',
+          fixtureData.length
+        )
+        cy.get('.lbh-list li').each((el, index) => {
+          const {
+            appointment: { start, end },
+            priority,
+            property,
+            propertyPostCode,
+            description,
+          } = fixtureData[index]
+          // Format start and end times for display as 'HH:mm – HH:mm'
+          const formattedTime = `${start} – ${end}`
+          // Verify that each element contains expected data from the fixture
+          cy.wrap(el).within(() => {
+            cy.contains(formattedTime)
+            cy.contains(priority.toLowerCase())
+            cy.contains(property)
+            cy.contains(propertyPostCode)
+            cy.contains(description)
+          })
+        })
       })
     })
   })
 
   context('When operative selects different date', () => {
-    //Date formatted to send to API
-    const targetDateForAPI = '2024-11-04'
-
-    //Date formatted to match UI
-    const datePickerDate = 'Nov 04'
-
     it('Changes the work orders', () => {
       cy.intercept(
         {
           method: 'GET',
-          path: `/api/operatives/017233/workOrdersNew?date=${targetDateForAPI}`,
+          path: `/api/operatives/017233/workOrdersNew*`,
         },
         {
           fixture: 'pastWorkOrders/4thNovemberpastWorkOrders.json',
         }
       ).as('pastOperativesWorkOrders')
       cy.visit('/oldjobs')
-      cy.get('.date-picker-container').within(() => {
-        cy.get('#date-picker').select(1)
-        cy.get('#date-picker').contains(`${datePickerDate}`)
-      })
       cy.wait('@pastOperativesWorkOrders').then((interception) => {
         const fixtureData = interception.response.body
         cy.get('.operative-work-order-list-item').should(
@@ -239,9 +261,7 @@ context('When an operative is logged in', () => {
             propertyPostCode,
             description,
           } = fixtureData[index]
-          //     // Format start and end times for display as 'HH:mm – HH:mm'
           const formattedTime = `${start} – ${end}`
-          //     // Verify that each element contains expected data from the fixture
           cy.wrap(el).within(() => {
             cy.contains(formattedTime)
             cy.contains(priority.toLowerCase())
@@ -257,13 +277,11 @@ context('When an operative is logged in', () => {
   context(
     "When they don't have any work orders on a particular date in the past",
     () => {
-      const targetDateForAPI = '2024-11-05'
-
       it('Displays a warning info box', () => {
         cy.intercept(
           {
             method: 'GET',
-            path: `/api/operatives/017233/workOrdersNew?date=${targetDateForAPI}`,
+            path: `/api/operatives/017233/workOrdersNew*`,
           },
           {
             body: [],
