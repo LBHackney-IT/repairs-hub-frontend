89842413bc064829e962f7b102e7bda04e27e34f
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2024-11-14 09:30:04 +0000

    rename component

--- src/components/PastWorkOrdersDatePicker/Index.tsx
+++ src/components/PastWorkOrdersDatePicker/Index.tsx
@@ -1,16 +1,22 @@
-interface DatePickerProps {
-  options: string[]
+import { beginningOfDay, daysBeforeDateRangeExcWeekend } from '@/utils/time'
+
+interface PastWorkOrdersDatePickerProps {
   handleChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-const DatePicker = ({ options, handleChange }: DatePickerProps) => {
+const PastWorkOrdersDatePicker = ({
+  handleChange,
+}: PastWorkOrdersDatePickerProps) => {
+  const currentDate = beginningOfDay(new Date())
+  const lastSevenDays = daysBeforeDateRangeExcWeekend(currentDate, 7)
+
   return (
     <div className="date-picker-container">
       <label htmlFor="date-picker" className="lbh-heading-h2">
         Select date
       </label>
       <select id="date-picker" name="date-picker" onChange={handleChange}>
-        {options.map((day, index) => {
+        {lastSevenDays.map((day, index) => {
           return (
             <option value={day} key={index}>
               {day.toString().slice(3, 10)}
@@ -22,4 +28,4 @@ const DatePicker = ({ options, handleChange }: DatePickerProps) => {
   )
 }
 
-export default DatePicker
+export default PastWorkOrdersDatePicker
