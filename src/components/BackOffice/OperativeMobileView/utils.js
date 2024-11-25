1934a6a7a152ac11e48f8f7d239d03bf82000eef
Adam <adam.tracy20@gmail.com>
2024-11-25 16:18:29 +0000

    Case insensitive filter

--- src/components/BackOffice/OperativeMobileView/utils.js
+++ src/components/BackOffice/OperativeMobileView/utils.js
@@ -1,6 +1,6 @@
 export const filterOperatives = (operatives, operativeFilter) => {
   if (!operativeFilter) return operatives;
-
+  operativeFilter = operativeFilter.toLowerCase();
   return operatives.filter((x) =>
     x.id?.toString().includes(operativeFilter) ||
     x.name?.toLowerCase().includes(operativeFilter) ||
