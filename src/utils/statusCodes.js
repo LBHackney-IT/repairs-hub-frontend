ab8cf378687a4867f5d4d6acf313ec4db45de8af
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2024-11-25 14:12:49 +0000

    lowercase

--- src/utils/statusCodes.js
+++ src/utils/statusCodes.js
@@ -86,7 +86,7 @@ export const FOLLOW_ON_REQUEST_AVAILABLE_TRADES = [
   { name: 'followon-trades-UPVC', label: 'UPVC', value: 'UPVC' },
   {
     name: 'followon-trades-other',
-    label: 'Other (Please specify in the text box below)',
+    label: 'Other (please specify in the text box below)',
     value: 'Other',
   },
 ]
