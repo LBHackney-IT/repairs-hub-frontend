7a4af59de4ae193dbedffea0a88b028f5d1f5d19
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2024-12-04 17:11:30 +0000

    add default option

--- src/components/Property/WorkOrdersHistoryFilter/Index.tsx
+++ src/components/Property/WorkOrdersHistoryFilter/Index.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { frontEndApiRequest } from '@/utils/frontEndApiClient/requests'
 
 interface WorkOrdersHistoryFilterProps {
   handleChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
   clearFilters: () => void
+  isUnselectable: boolean
 }
 
 const WorkOrdersHistoryFilter = ({
@@ -11,6 +12,7 @@ const WorkOrdersHistoryFilter = ({
   clearFilters,
 }: WorkOrdersHistoryFilterProps) => {
   const [trades, setTrades] = useState(null)
+  const selectRef = useRef<HTMLSelectElement>(null)
 
   const getTrades = async () => {
     try {
@@ -27,6 +29,14 @@ const WorkOrdersHistoryFilter = ({
     getTrades()
   }, [])
 
+  const handleClearFilters = (e: React.MouseEvent) => {
+    e.preventDefault()
+    if (selectRef.current) {
+      selectRef.current.value = ''
+      clearFilters()
+    }
+  }
+
   return (
     <div className="trade-picker-container">
       <label htmlFor="trade-picker" className="govuk-label lbh-label">
@@ -34,19 +44,21 @@ const WorkOrdersHistoryFilter = ({
       </label>
       {trades !== null && (
         <select
+          ref={selectRef}
           id="trade-picker"
           className="govuk-select lbh-select"
           name="trade-picker"
           onChange={handleChange}
           data-testid="trade-picker"
         >
+          <option value="">Select a trade</option>
           {trades.map((trade, index) => {
             const tradeName = trade.description
             return (
               <option
                 data-testid={`trade-option`}
-                value={trade.name}
-                key={trade.name}
+                value={tradeName}
+                key={tradeName}
                 className="govuk-select lbh-select"
               >
                 {tradeName}
@@ -58,7 +70,7 @@ const WorkOrdersHistoryFilter = ({
       <a
         className="lbh-link lbh-body-xs"
         href="#work-orders-history-tab"
-        onClick={clearFilters}
+        onClick={handleClearFilters}
       >
         Clear filters
       </a>
