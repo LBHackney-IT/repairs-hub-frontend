2e87449c875ee6184a0d41977f123a8b5f30387d
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-02-10 10:22:48 +0000

    write some e2e tests

---
+++ cypress/e2e/work-order/edit.cy.js
@@ -0,0 +1,185 @@
+/// <reference types="cypress" />
+import 'cypress-audit/commands'
+describe('Editing a work order description', () => {
+  context('As an authorisation manager', () => {
+    beforeEach(() => {
+      cy.loginWithAuthorisationManagerRole()
+      cy.fixture('workOrders/workOrder.json')
+        .then((workOrder) => {
+          workOrder.reference = 10000040
+          cy.intercept(
+            { method: 'GET', path: '/api/workOrders/10000040' },
+            { body: workOrder }
+          )
+        })
+        .as('workOrder')
+      cy.fixture('workOrders/editedWorkOrder.json')
+        .then((workOrder) => {
+          workOrder.reference = 10000040
+          cy.intercept(
+            { method: 'GET', path: '/api/workOrders/10000040' },
+            { body: workOrder }
+          )
+        })
+        .as('editedWorkOrder')
+      cy.intercept({
+        method: 'GET',
+        path: 'api/workOrders/10000040/tasks',
+      }).as('tasks')
+      cy.visit('/work-orders/10000040')
+    })
+    it('allows me to edit the work order description and adds the updated description to the notes', () => {
+      cy.get('[data-testid="details"] > .govuk-button').click()
+      cy.get('#workOrderMenu-2').click()
+      cy.get('.MultiButton_button__ApRbt').click()
+      cy.get('[data-testid="editRepairDescription"]')
+        .clear()
+        .type('This is a test description.')
+      cy.intercept({
+        method: 'PATCH',
+        path: '/api/workOrders/updateDescription',
+      }).as('updateDescription')
+      cy.get('.govuk-form-group > .govuk-button').click()
+      cy.get('.lbh-body-m').should('contain', 'This is a test description.')
+      cy.get('#tab_notes-tab').click()
+      cy.get('[data-note-id="0"] > span').should(
+        'contain',
+        'Description updated: This is a test description.'
+      )
+      cy.visit('/work-orders/10000040')
+    })
+    it('cancels description update when cancel button is clicked', () => {
+      cy.get('[data-testid="details"] > .govuk-button').click()
+      cy.get('#workOrderMenu-2').click()
+      cy.get('.MultiButton_button__ApRbt').click()
+      cy.get('.govuk-button-secondary').click()
+      cy.url().should('include', '/work-orders/10000040')
+    })
+  })
+  context('As a contract manager', () => {
+    beforeEach(() => {
+      cy.loginWithContractManagerRole()
+      cy.fixture('workOrders/workOrder.json')
+        .then((workOrder) => {
+          workOrder.reference = 10000040
+          cy.intercept(
+            { method: 'GET', path: '/api/workOrders/10000040' },
+            { body: workOrder }
+          )
+        })
+        .as('workOrder')
+      cy.fixture('workOrders/editedWorkOrder.json')
+        .then((workOrder) => {
+          workOrder.reference = 10000040
+          cy.intercept(
+            { method: 'GET', path: '/api/workOrders/10000040' },
+            { body: workOrder }
+          )
+        })
+        .as('editedWorkOrder')
+      cy.intercept({
+        method: 'GET',
+        path: 'api/workOrders/10000040/tasks',
+      }).as('tasks')
+      cy.intercept({
+        method: 'GET',
+        path: 'api/properties/00012345',
+      })
+      cy.visit('/work-orders/10000040')
+    })
+    it('allows me to edit the work order description and adds the updated description to the notes', () => {
+      cy.get('[data-testid="details"] > .govuk-button').click()
+      cy.get('#workOrderMenu-4').click()
+      cy.get('.MultiButton_button__ApRbt').click()
+      cy.get('[data-testid="editRepairDescription"]')
+        .clear()
+        .type('This is a test description.')
+      cy.get('.govuk-form-group > .govuk-button').click()
+      cy.get('.lbh-body-m').should('contain', 'This is a test description.')
+      cy.get('#tab_notes-tab').click()
+      cy.get('[data-note-id="0"] > span').should(
+        'contain',
+        'Description updated: This is a test description.'
+      )
+      cy.visit('/work-orders/10000040')
+    })
+    it('cancels description update when cancel button is clicked', () => {
+      cy.get('[data-testid="details"] > .govuk-button').click()
+      cy.get('#workOrderMenu-2').click()
+      cy.get('.MultiButton_button__ApRbt').click()
+      cy.get('.govuk-button-secondary').click()
+      cy.url().should('include', '/work-orders/10000040')
+    })
+  })
+  context('As a data admin', () => {
+    beforeEach(() => {
+      cy.loginWithDataAdminRole()
+      cy.fixture('workOrders/workOrder.json')
+        .then((workOrder) => {
+          workOrder.reference = 10000040
+          cy.intercept(
+            { method: 'GET', path: '/api/workOrders/10000040' },
+            { body: workOrder }
+          )
+        })
+        .as('workOrder')
+      cy.fixture('workOrders/editedWorkOrder.json')
+        .then((workOrder) => {
+          workOrder.reference = 10000040
+          cy.intercept(
+            { method: 'GET', path: '/api/workOrders/10000040' },
+            { body: workOrder }
+          )
+        })
+        .as('editedWorkOrder')
+      cy.intercept({
+        method: 'GET',
+        path: 'api/workOrders/10000040/tasks',
+      }).as('tasks')
+      cy.intercept({
+        method: 'GET',
+        path: 'api/properties/00012345',
+      }).as('property')
+      cy.visit('/backoffice')
+    })
+    it.only('allows me to edit the work order description', () => {
+      cy.get('[data-testid="details"] > .govuk-button').click()
+      cy.get('#workOrderMenu-4').click()
+      cy.get('.MultiButton_button__ApRbt').click()
+      cy.get('[data-testid="editRepairDescription"]')
+        .clear()
+        .type('This is a test description.')
+      cy.get('.govuk-form-group > .govuk-button').click()
+      cy.get('.lbh-body-m').should('contain', 'This is a test description.')
+    })
+  })
+  context('When user submits empty text field', () => {
+    it('Shows an error', () => {
+      cy.loginWithAuthorisationManagerRole()
+      cy.intercept({
+        method: 'PATCH',
+        path: `/api/workOrders/updateDescription`,
+      })
+      cy.visit('/work-orders/10000040/edit')
+      cy.get('[data-testid="editRepairDescription"]').clear()
+      cy.get('.govuk-form-group > .govuk-button').click()
+      cy.contains('Please enter a repair description')
+    })
+  })
+  context('When user submits too many characters', () => {
+    it('Shows an error', () => {
+      const longDescription = new Array(240).join('a')
+      cy.loginWithAuthorisationManagerRole()
+      cy.intercept({
+        method: 'PATCH',
+        path: `/api/workOrders/updateDescription`,
+      })
+      cy.visit('/work-orders/10000040/edit')
+      cy.get('[data-testid="editRepairDescription"]')
+        .clear()
+        .type(longDescription)
+      cy.get('.govuk-form-group > .govuk-button').click()
+      cy.contains('You have exceeded the maximum amount of characters')
+    })
+  })
+})
