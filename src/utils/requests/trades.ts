07ed1abf1543f7049bb4d3dd25e8329188232afd
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-03-06 15:46:26 +0000

    run lintfix

--- src/utils/requests/trades.ts
+++ src/utils/requests/trades.ts
@@ -1,29 +1,29 @@
-import { ApiResponseType } from "../../types/requests/types"
-import { frontEndApiRequest } from "../frontEndApiClient/requests"
+import { ApiResponseType } from '../../types/requests/types'
+import { frontEndApiRequest } from '../frontEndApiClient/requests'
 
 export const getTrades = async (): Promise<ApiResponseType> => {
-    try {
-      const workOrderFilters = await frontEndApiRequest({
-        method: 'get',
-        path: '/api/filter/WorkOrder',
-      })
+  try {
+    const workOrderFilters = await frontEndApiRequest({
+      method: 'get',
+      path: '/api/filter/WorkOrder',
+    })
 
-      return {
-        success: true,
+    return {
+      success: true,
       response: workOrderFilters.Trades,
       error: null,
-      }
-    } catch (e) {
-      console.error('An error has occured:', e.response)
-      return {
-        success: false,
-        response: null,
-        error:
-          e.response?.status === 400
-            ? `Invalid request data`
-            : `Oops, an error occurred: ${
-                e.response?.status
-              } with message: ${JSON.stringify(e.response?.data?.message)}`,
-      }
+    }
+  } catch (e) {
+    console.error('An error has occured:', e.response)
+    return {
+      success: false,
+      response: null,
+      error:
+        e.response?.status === 400
+          ? `Invalid request data`
+          : `Oops, an error occurred: ${
+              e.response?.status
+            } with message: ${JSON.stringify(e.response?.data?.message)}`,
     }
   }
+}
