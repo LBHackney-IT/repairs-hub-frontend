6519d57dc4080079e78a7a1583e8274dd61fe12b
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2025-03-24 17:09:00 +0000

    Add tests for ScheduleAppointment component

--- src/components/WorkOrder/ScheduleAppointment/ScheduleAppointment.tsx
+++ src/components/WorkOrder/ScheduleAppointment/ScheduleAppointment.tsx
@@ -23,21 +23,26 @@ const ScheduleAppointment = ({
     handleExternalLinkOpen,
   } = useDrsAppointmentScheduler(workOrderReference)
 
-  if (isLoading)
-    <span
-      style={{
-        color: '#64748b',
-        fontSize: '12px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-      }}
-    >
-      <Spinner width={20} height={20} />
-      <span style={{ margin: '0 0 0 10px' }}>Fetching DRS session...</span>
-    </span>
+  if (isLoading) {
+    return (
+      <span
+        style={{
+          color: '#64748b',
+          fontSize: '12px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Spinner width={20} height={20} />
+        <span style={{ margin: '0 0 0 10px' }}>Fetching DRS session...</span>
+      </span>
+    )
+  }
 
-  if (error) return <ErrorMessage label={error} />
+  if (error) {
+    return <ErrorMessage label={error} />
+  }
 
   return (
     <ScheduleDRSAppointmentLink
