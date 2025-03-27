afa9570935e27e44e1dac985f9d8f07ebddf0475
AlexpdHackney <alexander.perez-davies@hackney.gov.uk>
2025-03-27 12:01:24 +0000

    Add estimated duration radio buttons (#1024)
    * add time estimtation radio buttons
    
    * sends estimatedDuration to api
    
    * estimated duration added to summary
    
    * estimated duration added to note
    
    * add estimated duration to operative page
    
    * add data-test id to radio buttons
    
    * estimated duration added to follow on request
    
    * run lintfix
    
    * fix tests
    
    * tests pass
    
    * tests pass
    
    * remove only
    
    * make radio options like design
    
    * styled correctly
    
    * fix css

--- src/components/Form/Radios/index.tsx
+++ src/components/Form/Radios/index.tsx
@@ -24,6 +24,7 @@ interface Props {
   children?: JSX.Element
   required?: boolean
   isRadiosInline?: boolean
+  isGrid?: boolean
   [key: string]: any
 }
 
@@ -40,6 +41,7 @@ const Radio = (props: Props) => {
     children,
     required,
     isRadiosInline = false,
+    isGrid,
     ...otherProps
   } = props
 
@@ -70,6 +72,12 @@ const Radio = (props: Props) => {
         className={cx('govuk-radios lbh-radios', {
           'govuk-radios--inline': isRadiosInline,
         })}
+        style={
+          isGrid && {
+            display: 'inline-grid',
+            gridTemplateColumns: '1fr 1fr',
+          }
+        }
       >
         {options.map((option) => {
           const { value, text, defaultChecked, hint } =
