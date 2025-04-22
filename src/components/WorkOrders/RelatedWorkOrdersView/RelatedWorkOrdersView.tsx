5367555d1ce5c76317f1c38a3f73d8d022f893ad
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2025-04-22 13:52:42 +0100

    Add snapshot test

--- src/components/WorkOrders/RelatedWorkOrdersView/RelatedWorkOrdersView.tsx
+++ src/components/WorkOrders/RelatedWorkOrdersView/RelatedWorkOrdersView.tsx
@@ -2,21 +2,8 @@ import { useState, useEffect } from 'react'
 import Spinner from '../../Spinner'
 import ErrorMessage from '../../Errors/ErrorMessage'
 import { frontEndApiRequest } from '@/utils/frontEndApiClient/requests'
-import { WorkOrder } from '@/root/src/models/workOrder'
-import Link from 'next/link'
-import { format } from 'date-fns'
-import Status from '../../WorkOrder/Status'
-import classNames from 'classnames'
-
-interface WorkOrderHierarchy {
-  rootParentId: string
-  workOrders: {
-    directParentId: string
-    isRoot: boolean
-    isSelf: boolean
-    workOrder: WorkOrder
-  }[]
-}
+import { WorkOrderHierarchy } from './types'
+import RelatedWorkOrderViewList from './RelatedWorkOrderViewList'
 
 interface Props {
   tabName: string
@@ -62,57 +49,14 @@ const RelatedWorkOrdersView = ({ workOrderReference, tabName }: Props) => {
     return <Spinner />
   }
 
+  if (error) {
+    return <ErrorMessage label={error} />
+  }
+
   return (
     <>
       <h2 className="lbh-heading-h2">{tabName}</h2>
-      {hierarchy && (
-        <ul className="lbh-body-m wo-hierarchy-list">
-          {hierarchy?.workOrders?.map(({ workOrder, isSelf }) => (
-            <li
-              className={classNames('wo-hierarchy-list-row', {
-                'wo-hierarchy-list-row--self': isSelf,
-              })}
-              key={workOrder.reference}
-            >
-              <div className="wo-hierarchy-row-left">
-                <span className="wo-hierarchy-left-bar" />
-                <span className="wo-hierarchy-left-circle" />
-              </div>
-
-              <div className="wo-hierarchy-row-right">
-                <div className="wo-hierarchy-bottom-container">
-                  <div className="wo-hierarchy-date">
-                    {format(new Date(workOrder.dateRaised), 'd MMMM yyyy')}
-                  </div>
-
-                  <div style={{ marginTop: 0 }}>
-                    <Status
-                      text={workOrder.status}
-                      className="work-order-status"
-                    />
-                  </div>
-                </div>
-                <div className="wo-hierarchy-link-and-trade">
-                  <span className="wo-hierarchy-link">
-                    <Link href={`/work-orders/${workOrder.reference}`}>
-                      {workOrder.reference}
-                    </Link>
-                  </span>
-
-                  <div style={{ marginTop: 0 }}>
-                    {workOrder.tradeDescription}
-                  </div>
-                </div>
-
-                <p style={{ marginTop: 0 }}>{workOrder.description}</p>
-                <hr className="wo-hierarchy-hr" />
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {error && <ErrorMessage label={error} />}
+      {hierarchy && <RelatedWorkOrderViewList hierarchy={hierarchy} />}
     </>
   )
 }
