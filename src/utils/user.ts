88de80651c1f0bfb2d51ff08f597ce1263a7d026
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <88662046+LBHCallumM@users.noreply.github.com>
2025-04-28 10:08:40 +0100

    Add follow-on section to CreateWorkOrder form (#1048)
    * initial
    
    * lf
    
    * Add user group
    
    * Wrap follow-on form fields in authorizer
    
    * wip - e2e tests
    
    * Update tests
    
    * refactor
    
    * fix eslint issue
    
    * cleanup for pr

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -5,6 +5,7 @@ export const AUTHORISATION_MANAGER_ROLE = 'authorisation_manager'
 export const OPERATIVE_ROLE = 'operative'
 export const BUDGET_CODE_OFFICER_ROLE = 'budget_code_officer'
 export const DATA_ADMIN_ROLE = 'data_admin'
+export const FOLLOWON_ADMIN_ROLE = 'followon_admin'
 
 export const ALL_ROLES = [
   AGENT_ROLE,
@@ -14,22 +15,28 @@ export const ALL_ROLES = [
   OPERATIVE_ROLE,
   BUDGET_CODE_OFFICER_ROLE,
   DATA_ADMIN_ROLE,
+  FOLLOWON_ADMIN_ROLE,
 ]
 
-export const buildUser = (name, email, authServiceGroups) => {
+export const buildUser = (
+  name: string,
+  email: string,
+  authServiceGroups: string[]
+) => {
   const {
     CONTRACT_MANAGERS_GOOGLE_GROUPNAME,
     AUTHORISATION_MANAGERS_GOOGLE_GROUPNAME,
     OPERATIVES_GOOGLE_GROUPNAME,
     BUDGET_CODE_OFFICER_GOOGLE_GROUPNAME,
     DATA_ADMIN_GOOGLE_GROUPNAME,
+    FOLLOWON_ADMIN_GOOGLE_GROUPNAME,
   } = process.env
 
   const CONTRACTORS_GOOGLE_GROUPNAME_REGEX =
-    process.env.CONTRACTORS_GOOGLE_GROUPNAME_REGEX || false
+    process.env.CONTRACTORS_GOOGLE_GROUPNAME_REGEX || ''
 
   const AGENTS_GOOGLE_GROUPNAME_REGEX =
-    process.env.AGENTS_GOOGLE_GROUPNAME_REGEX || false
+    process.env.AGENTS_GOOGLE_GROUPNAME_REGEX || ''
 
   const contractorGroupRegex = new RegExp(CONTRACTORS_GOOGLE_GROUPNAME_REGEX)
   const agentGroupRegex = new RegExp(AGENTS_GOOGLE_GROUPNAME_REGEX)
@@ -50,31 +57,37 @@ export const buildUser = (name, email, authServiceGroups) => {
         return BUDGET_CODE_OFFICER_ROLE
       } else if (isDataAdminGroupName(groupName)) {
         return DATA_ADMIN_ROLE
+      } else if (isFollowOnAdminGroupName(groupName)) {
+        return FOLLOWON_ADMIN_ROLE
       }
 
-      console.log(`User group name not recognised: ${groupName}`)
+      console.warn(`User group name not recognised: ${groupName}`)
     })
   }
 
-  const isAgentGroupName = (groupName) => !!agentGroupRegex.test(groupName)
+  const isAgentGroupName = (groupName: string) =>
+    !!agentGroupRegex.test(groupName)
 
-  const isContractorGroupName = (groupName) =>
+  const isContractorGroupName = (groupName: string) =>
     !!contractorGroupRegex.test(groupName)
 
-  const isContractManagerGroupName = (groupName) =>
+  const isContractManagerGroupName = (groupName: string) =>
     groupName === CONTRACT_MANAGERS_GOOGLE_GROUPNAME
 
-  const isAuthorisationManagerGroupName = (groupName) =>
+  const isAuthorisationManagerGroupName = (groupName: string) =>
     groupName === AUTHORISATION_MANAGERS_GOOGLE_GROUPNAME
 
-  const isOperativeGroupName = (groupName) =>
+  const isOperativeGroupName = (groupName: string) =>
     groupName === OPERATIVES_GOOGLE_GROUPNAME
 
-  const isBudgetCodeOfficerGroupName = (groupName) =>
+  const isBudgetCodeOfficerGroupName = (groupName: string) =>
     groupName === BUDGET_CODE_OFFICER_GOOGLE_GROUPNAME
 
-  const isDataAdminGroupName = (groupName) =>
+  const isDataAdminGroupName = (groupName: string) =>
     groupName === DATA_ADMIN_GOOGLE_GROUPNAME
+
+  const isFollowOnAdminGroupName = (groupName: string) =>
+    groupName === FOLLOWON_ADMIN_GOOGLE_GROUPNAME
 
   const groupNames = authServiceGroups.filter(
     (groupName) =>
@@ -84,12 +97,13 @@ export const buildUser = (name, email, authServiceGroups) => {
       isContractorGroupName(groupName) ||
       isOperativeGroupName(groupName) ||
       isBudgetCodeOfficerGroupName(groupName) ||
-      isDataAdminGroupName(groupName)
+      isDataAdminGroupName(groupName) ||
+      isFollowOnAdminGroupName(groupName)
   )
 
   const roles = rolesFromGroups(groupNames)
 
-  const hasRole = (r) => roles.includes(r)
+  const hasRole = (r: string) => roles.includes(r)
 
   return {
     name: name,
@@ -104,5 +118,6 @@ export const buildUser = (name, email, authServiceGroups) => {
     hasAnyPermissions: roles.length > 0,
     hasBudgetCodeOfficerPermissions: hasRole(BUDGET_CODE_OFFICER_ROLE),
     hasDataAdminPermissions: hasRole(DATA_ADMIN_ROLE),
+    hasFollowOnAdminPermissions: hasRole(FOLLOWON_ADMIN_ROLE),
   }
 }
