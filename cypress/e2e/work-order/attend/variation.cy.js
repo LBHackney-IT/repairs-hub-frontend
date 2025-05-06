6a46cb3580938916c40ad938860be93fba894980
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2025-05-06 15:46:26 +0100

    missed intercept

--- cypress/e2e/work-order/attend/variation.cy.js
+++ cypress/e2e/work-order/attend/variation.cy.js
@@ -4,7 +4,7 @@ import 'cypress-audit/commands'
 
 context('when a variation is made', () => {
   beforeEach(() => {
-    cy.intercept('/api/workOrders/10000621', {
+    cy.intercept('/api/workOrders/10000621/new', {
       fixture: 'workOrders/workOrder.json',
     }).as('workOrderRequest')
 
