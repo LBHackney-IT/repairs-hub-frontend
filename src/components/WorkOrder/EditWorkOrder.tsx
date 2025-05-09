c42d6bea9977806ee2646463bc23f8455a099407
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <88662046+LBHCallumM@users.noreply.github.com>
2025-05-09 16:23:40 +0100

    Update work order components to typescript (#1064)
    * convert work order components to typescript
    
    * update tests
    
    * update tests
    
    * try fix
    
    * change name
    
    * change name back
    
    * missed import
    
    * more fixes
    
    * revert
    
    * updates
    
    * dfgdggdfgfg
    
    * sdjfojsdhflsdf
    
    * update snapshots

--- src/components/WorkOrder/EditWorkOrder.tsx
+++ src/components/WorkOrder/EditWorkOrder.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 import { GridRow, GridColumn } from '../Layout/Grid'
--- src/components/WorkOrder/Operatives.tsx
+++ src/components/WorkOrder/Operatives.tsx
@@ -1,4 +1,10 @@
-const Operatives = ({ operatives }) => {
+import { Operative } from '../../models/operativeModel'
+
+interface Props {
+  operatives: Operative[]
+}
+
+const Operatives = ({ operatives }: Props) => {
   return (
     <p className="lbh-body-xs">
       {`${operatives.length > 1 ? 'Operatives' : 'Operative'}: ${operatives
