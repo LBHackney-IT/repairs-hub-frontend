76b101f81e2782c69a5cb29fd51c02f4e8278041
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <88662046+LBHCallumM@users.noreply.github.com>
2025-05-14 15:26:21 +0100

    Try new appointment details endpoint for workorder view page (#1067)
    * try new appointment endpoint for workorder view page
    
    * Add feature toggle to new endpoint
    
    * update tests
    
    * update tests
    
    * fix authorization tests
    
    * Fix appointment show tests
    
    * update tests
    
    * last tests
    
    * lf

--- cypress/e2e/work-order/show/tabs/tasks-and-sors.cy.js
+++ cypress/e2e/work-order/show/tabs/tasks-and-sors.cy.js
@@ -7,9 +7,17 @@ describe('Tasks and SORs', () => {
     cy.loginWithAgentRole()
 
     cy.intercept(
-      { method: 'GET', path: '/api/workOrders/10000012' },
+      { method: 'GET', path: '/api/workOrders/10000012/new' },
       { fixture: 'workOrders/workOrder.json' }
     )
+
+    cy.intercept(
+      { method: 'GET', path: '/api/workOrders/appointments/10000012' },
+      {
+        fixture: 'workOrderAppointments/noAppointment.json',
+      }
+    )
+
     cy.intercept(
       { method: 'GET', path: '/api/properties/00012345' },
       { fixture: 'properties/property.json' }
