77ef0157312ec16b67b6a7c6f4f5eae8ac1690f1
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-05-14 15:52:34 +0100

    add intercepts

--- cypress/e2e/work-order/edit.cy.js
+++ cypress/e2e/work-order/edit.cy.js
@@ -12,47 +12,60 @@ describe('Editing a work order description', () => {
         { method: 'GET', path: '/api/workOrders/10000012/tasks' },
         { fixture: 'workOrders/task.json' }
       ).as('tasks')
+      cy.intercept(
+        {
+          method: 'GET',
+          path:
+            '/api/properties/4552c539-2e00-8533-078d-9cc59d9115da/person-alerts',
+        },
+        { fixture: 'properties/personAlerts.json' }
+      ).as('personAlerts')
+      cy.intercept(
+        {
+          method: 'GET',
+          path: '/api/properties/00012345/location-alerts',
+        },
+        { fixture: 'properties/personAlerts.json' }
+      ).as('locationAlerts')
       cy.visit('/work-orders/10000012')
     })
     it.only('allows me to edit the work order and adds the updated description to the notes', () => {
       cy.get('[data-testid="details"] > .govuk-button').click()
       cy.get('#workOrderMenu-2').click()
-      // cy.intercept(
-      //   { method: 'GET', path: '/api/properties/00012345' },
-      //   { fixture: 'properties/property.json' }
-      // ).as('property')
-      // cy.intercept(
-      //   {
-      //     method: 'GET',
-      //     path: '/api/contact-details/4552c539-2e00-8533-078d-9cc59d9115da',
-      //   },
-      //   { fixture: 'contactDetails/contactDetails.json' }
-      // ).as('contactDetails')
+      cy.intercept(
+        { method: 'GET', path: '/api/properties/00012345' },
+        { fixture: 'properties/property.json' }
+      ).as('property')
+      cy.intercept(
+        {
+          method: 'GET',
+          path: '/api/contact-details/4552c539-2e00-8533-078d-9cc59d9115da',
+        },
+        { fixture: 'contactDetails/contactDetails.json' }
+      ).as('contactDetails')
       cy.get('.MultiButton_button__ApRbt').click()
-      // cy.wait('@property')
-      // cy.wait('@contactDetails')
       cy.get('[data-testid="editRepairDescription"]')
         .clear()
         .type('This is a test description.')
       cy.get('[data-testid="callerName"]').clear().type('Test Name')
       cy.get('[data-testid="contactNumber"]').clear().type('01234567890')
+      cy.get('.govuk-form-group > .govuk-button').click()
       cy.intercept({
         method: 'PATCH',
         path: '/api/workOrders/editWorkOrder',
       }).as('editWorkOrder')
-      cy.fixture('workOrders/editedWorkOrder.json')
-        .then((workOrder) => {
-          cy.intercept(
-            { method: 'GET', path: '/api/workOrders/10000012' },
-            { body: workOrder }
-          )
-        })
-        .as('editedWorkOrder')
-      cy.get('.govuk-form-group > .govuk-button').click()
+      cy.intercept(
+        {
+          method: 'GET',
+          path: '/api/workOrders/10000012',
+        },
+        { fixture: 'workorders/editedWorkOrder.json' }
+      ).as('editedWorkOrder')
       cy.get('.lbh-body-m').should('contain', 'This is a test description.')
       cy.contains('Test Name')
       cy.contains('01234567890')
       cy.get('#tab_notes-tab').click()
+
       cy.get('[data-note-id="0"] > span').should(
         'contain',
         'Description updated: This is a test description.'
