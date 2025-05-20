b5025cb6ed8710f5c97af46281be40c71c60ebd8
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <88662046+LBHCallumM@users.noreply.github.com>
2025-05-20 17:03:49 +0100

    Update Mobile Variation form (#1079)
    * WIP
    
    * lf
    
    * fix that annoying error
    
    * cleanup
    
    * update tests
    
    * rm .only
    
    * update message
    
    * toggle pipeline
    
    * lf
    
    * Disable add operatives button, this also breaks when pending variation
    
    * PR changes
    
    * revert pipeline
    
    * Fix broken test

---
+++ src/components/Operatives/NewTaskForm.tsx
@@ -0,0 +1,23 @@
+import WorkOrderUpdateView from '../WorkOrder/Update'
+import { mobileViewUpdateWorkOrderLinks } from '../../utils/successPageLinks'
+
+interface Props {
+  workOrderReference: string
+  operativePayrollNumber: string
+}
+
+const NewTaskForm = (props: Props) => {
+  const { workOrderReference, operativePayrollNumber } = props
+
+  return (
+    <WorkOrderUpdateView
+      reference={workOrderReference}
+      mobileViewLinks={mobileViewUpdateWorkOrderLinks(
+        operativePayrollNumber,
+        workOrderReference
+      )}
+    />
+  )
+}
+
+export default NewTaskForm
