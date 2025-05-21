8b0e613bd3ad31e0a2df9b06be63d42ff78ac607
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2025-05-21 11:13:32 +0100

    LF

--- src/components/WorkOrders/ConfirmCloseWorkOrderView/index.tsx
+++ src/components/WorkOrders/ConfirmCloseWorkOrderView/index.tsx
@@ -33,7 +33,8 @@ const ConfirmCloseWorkOrderView = (props: Props) => {
     setLoadingStatus('Fetching work order data')
 
     const workOrderResponse: ApiResponseType<WorkOrder> = await getWorkOrder(
-      workOrderId, false
+      workOrderId,
+      false
     )
 
     if (workOrderResponse.success) {
