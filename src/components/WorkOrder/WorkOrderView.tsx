d15b504a874eac9f5b5611d025db89048db45061
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2025-05-23 10:48:54 +0100

    fix build

--- src/components/WorkOrder/WorkOrderView.tsx
+++ src/components/WorkOrder/WorkOrderView.tsx
@@ -9,7 +9,7 @@ import PrintJobTicketDetails from './PrintJobTicketDetails'
 import WorkOrderViewTabs from '../Tabs/Views/WorkOrderViewTabs'
 import { CautionaryAlert } from '../../models/cautionaryAlerts'
 import { Tenure } from '../../models/tenure'
-import { getWorkOrderNew } from '../../utils/requests/workOrders'
+import { getWorkOrder } from '../../utils/requests/workOrders'
 import { APIResponseError } from '../../types/requests/types'
 import { Property } from '../../models/property'
 
