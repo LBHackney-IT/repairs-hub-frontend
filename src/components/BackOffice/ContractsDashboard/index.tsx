c1ae8201553d96479fd9c9a0fcec89e5073ea1a9
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-06-12 18:38:37 +0100

    fetch and paginate contracts

---
+++ src/components/BackOffice/ContractsDashboard/index.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+
+import Layout from '../Layout'
+import Spinner from '../../Spinner'
+import ErrorMessage from '../../Errors/ErrorMessage'
+import WarningInfoBox from '../../Template/WarningInfoBox'
+import { ContractsListItems } from './ContractsListItems'
+import { getContracts } from '@/root/src/utils/requests/contracts'
+
+const ContractsDashboard = () => {
+  const router = useRouter()
+  const pageFromQuery = parseInt(router.query.page as string) || 1
+
+  const [isLoading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>()
+  const [contracts, setContracts] = useState<null | any[]>(null)
+  const [pageNumber, setPageNumber] = useState(pageFromQuery)
+  const pageSize = 10
+
+  useEffect(() => {
+    if (pageNumber !== pageFromQuery) {
+      setPageNumber(pageFromQuery)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.query.page])
+
+  // When user clicks next/prev, update the URL (which will update pageNumber via the effect above)
+  const handleSetPageNumber = (newPage: number) => {
+    router.replace(
+      {
+        pathname: router.pathname,
+        query: { ...router.query, page: newPage },
+      },
+      undefined,
+      { shallow: true }
+    )
+  }
+
+  const startIndex = (pageNumber - 1) * pageSize
+  const endIndex = startIndex + pageSize
+  const totalPages = Math.ceil(contracts?.length / 10)
+
+  const fetchContracts = async () => {
+    const contractsReponse = await getContracts()
+
+    if (!contractsReponse.success) {
+      setError(contractsReponse.error?.message)
+      setLoading(false)
+      return
+    }
+    setContracts(contractsReponse.response)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchContracts()
+  }, [])
+
+  const filteredContracts = () => {
+    return contracts?.slice().reverse().slice(startIndex, endIndex)
+  }
+
+  return (
+    <Layout title="Contracts Dashboard">
+      {isLoading ? (
+        <Spinner />
+      ) : error ? (
+        <ErrorMessage label={error} />
+      ) : contracts?.length ? (
+        <ol className="lbh-list mobile-working-work-order-list">
+          <ContractsListItems
+            filteredContracts={filteredContracts()}
+            pageNumber={pageNumber}
+            setPageNumber={handleSetPageNumber}
+            totalPages={totalPages}
+          />
+        </ol>
+      ) : (
+        <WarningInfoBox
+          header="No work orders displayed"
+          name="No jobs warning"
+        />
+      )}
+    </Layout>
+  )
+}
+
+export default ContractsDashboard
