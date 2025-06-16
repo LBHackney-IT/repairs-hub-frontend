2543fa927243c92f990a31e5aa882708f5e47cf2
Adam <74552077+adamtry@users.noreply.github.com>
2025-06-16 10:10:51 +0100

    Add option for isEmergency to follow on request form (#1116)
    * Add option for isEmergency to follow on request form
    
    * Update value types for urgency options in FollowOnRequestTypeOfWorkForm
    
    * Fix routing after work order closure
    
    * Add urgency option to follow-on request form and update tests
    
    * Update follow-on request urgency and isEmergency fields in tests
    
    * Add feature toggle for Follow On emergency field and update related components
    
    * Feature toggle fallback
    
    * Simplify feature toggle flow
    
    * Fix test
    
    * Intercept feature toggle in close
    
    * Remove conditional rendering for emergency field toggle message
    
    * Urgency options Yes and No

--- src/components/WorkOrders/CloseWorkOrderFormComponents/FollowOnRequestTypeOfWorkForm.tsx
+++ src/components/WorkOrders/CloseWorkOrderFormComponents/FollowOnRequestTypeOfWorkForm.tsx
@@ -2,10 +2,32 @@ import classNames from 'classnames'
 import { Radio, TextArea } from '../../Form'
 import ErrorMessage from '../../Errors/ErrorMessage'
 import FollowOnRequestDifferentTradesForm from './FollowOnRequestDifferentTradesForm'
-import { FOLLOW_ON_REQUEST_AVAILABLE_TRADES } from '@/utils/statusCodes'
+import {
+  DeepMap,
+  FieldError,
+  FieldValues,
+  UseFormMethods,
+} from 'react-hook-form'
+import { followOnDataRequest } from '@/root/src/utils/hact/workOrderComplete/closeWorkOrder'
 import { useEffect, useState } from 'react'
+import { fetchSimpleFeatureToggles } from '@/root/src/utils/frontEndApiClient/requests'
+import { SimpleFeatureToggleResponse } from '@/root/src/pages/api/simple-feature-toggle'
 
-const FollowOnRequestTypeOfWorkForm = (props) => {
+interface FollowOnRequestTypeOfWorkFormProps {
+  errors: DeepMap<FieldValues, FieldError>
+  register: UseFormMethods['register']
+  getValues: (payload?: string | string[]) => unknown
+  setError: (name: string, error: FieldError) => void
+  clearErrors: (name?: string | string[]) => void
+  watch: CallableFunction
+  followOnData?: Partial<followOnDataRequest>
+  hasWhiteBackground?: boolean
+  isGrid?: boolean
+}
+
+const FollowOnRequestTypeOfWorkForm = (
+  props: FollowOnRequestTypeOfWorkFormProps
+): JSX.Element => {
   const {
     errors,
     register,
@@ -17,6 +39,30 @@ const FollowOnRequestTypeOfWorkForm = (props) => {
     hasWhiteBackground,
     isGrid,
   } = props
+
+  const [
+    simpleFeatureToggles,
+    setSimpleFeatureToggles,
+  ] = useState<SimpleFeatureToggleResponse>()
+
+  useEffect(() => {
+    fetchSimpleFeatureToggles().then((fetchedFeatureToggles) => {
+      setSimpleFeatureToggles(fetchedFeatureToggles)
+    })
+  }, [])
+
+  const URGENCY_OPTIONS = [
+    {
+      value: true,
+      text: 'Yes',
+      defaultChecked: followOnData?.isEmergency === true,
+    },
+    {
+      value: false,
+      text: 'No',
+      defaultChecked: followOnData?.isEmergency === false,
+    },
+  ]
 
   const MULTIPLE_OPERATIVES_REQUIRED_OPTIONS = [
     {
@@ -39,6 +85,23 @@ const FollowOnRequestTypeOfWorkForm = (props) => {
         className="govuk-fieldset govuk-!-margin-bottom-2 govuk-!-padding-2 lbh-fieldset"
         style={{ marginTop: 0 }}
       >
+        {simpleFeatureToggles?.enableFollowOnIsEmergencyField && (
+          <div style={{ marginBottom: '3rem' }}>
+            <Radio
+              data-testid="followonRequestUrgency"
+              labelSize="s"
+              label="Is this an emergency?"
+              name="isEmergency"
+              options={URGENCY_OPTIONS}
+              register={register({
+                required: 'Please confirm if this is an emergency',
+              })}
+              error={errors && errors.isEmergency}
+              hasWhiteBackground={hasWhiteBackground}
+            />
+          </div>
+        )}
+
         <div
           className={classNames('lbh-form-group', {
             'govuk-form-group--error': errors.typeOfWork,
