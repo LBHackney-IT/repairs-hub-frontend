c8dbbb85559ccd09723f31045520ebf1fea3381a
Adam <adam.tracy20@gmail.com>
2025-06-23 09:31:02 +0100

    Deactivate test error

--- src/components/WorkOrder/Photos/hooks/uploadFiles/uploadFileToS3.ts
+++ src/components/WorkOrder/Photos/hooks/uploadFiles/uploadFileToS3.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { Link } from '../types'
 
-const TEST_ERROR = true
+// const TEST_ERROR = true
 
 const uploadFileToS3 = async (file: File, link: Link): Promise<void> => {
-  if (TEST_ERROR) throw new Error('test upload error')
+  // if (TEST_ERROR) throw new Error('test upload error')
   await axios.request({
     method: 'PUT',
     url: link.presignedUrl,
