c1d447c6f0856fdc9174884c2aeefcfc54ed6379
AlexpdHackney <alexander.perez-davies@hackney.gov.uk>
2025-06-25 16:11:03 +0100

    Display contracts by contractor alphabetically (#1131)
    * rejig file structure and display contracts due to expire in 2 months
    
    * try fix linting issues
    
    * remove unecessary useEffect
    
    * change variable name
    
    * add warning info box name
    
    * display contracts by contractor
    
    * refactor conditional rendering
    
    * remove slice from merge
    
    * remove unused code
    
    * remove pagination
    
    * display contractors in alphabetical order
    
    * fix linter

--- src/components/BackOffice/ContractsDashboard/ContractorsListItems.tsx
+++ src/components/BackOffice/ContractsDashboard/ContractorsListItems.tsx
@@ -1,52 +1,68 @@
-import ContractorListItem from './ContractorListItem'
-import { Button } from '../../Form'
-
 import Contract from '@/root/src/models/contract'
+import ContractorListItem from './ContractorListItem'
+import WarningInfoBox from '../../Template/WarningInfoBox'
 
 interface ContractorsListItemsProps {
-  filteredContracts: Contract[]
-  setPageNumber: (pageNumber: number) => void
-  pageNumber: number
-  totalPages: number
+  contracts: Contract[]
 }
 
-const ContractorsListItems = ({
-  filteredContracts,
-  setPageNumber,
-  pageNumber,
-  totalPages,
-}: ContractorsListItemsProps) => {
-  if (filteredContracts === null || filteredContracts?.length === 0) {
-    return <></>
+const ContractorsListItems = ({ contracts }: ContractorsListItemsProps) => {
+  const relevantContracts = contracts?.filter((c) => c.terminationDate > '2020')
+
+  const contractorNames = new Set<string>(
+    relevantContracts.map((contract) => contract.contractorName)
+  )
+  const contractorReferences = new Set<string>(
+    relevantContracts.map((contract) => contract.contractorReference)
+  )
+
+  const contractorNamesAndReferences = [...contractorNames].map(
+    (name, index) => ({
+      contractorName: name,
+      contractorReference: [...contractorReferences][index],
+    })
+  )
+
+  const contractorNamesAndReferencesSortedByContractorName = contractorNamesAndReferences.sort(
+    (a, b) => a.contractorName.localeCompare(b.contractorName)
+  )
+
+  if (
+    !contractorNamesAndReferencesSortedByContractorName ||
+    Object.keys(contractorNamesAndReferencesSortedByContractorName).length === 0
+  ) {
+    return (
+      <>
+        <h3 className="lbh-heading-h3 lbh-!-font-weight-bold govuk-!-margin-bottom-1">
+          Contractors:
+        </h3>
+        <div style={{ width: '85%' }}>
+          <WarningInfoBox
+            header="No contractors found!"
+            text="Problem loading contractors."
+            name="no-contractors-found"
+          />
+        </div>
+      </>
+    )
   }
 
   return (
     <div>
+      <h3 className="lbh-heading-h3 lbh-!-font-weight-bold govuk-!-margin-bottom-1">
+        Contractors:
+      </h3>
       <ol className="lbh-list mobile-working-work-order-list">
-        {filteredContracts.map((contract) => (
-          <ContractorListItem
-            contractorReference={contract.contractorReference}
-            key={contract.contractReference}
-          />
-        ))}
+        {contractorNamesAndReferencesSortedByContractorName.map(
+          (obj, index) => (
+            <ContractorListItem
+              contractorReference={obj.contractorReference}
+              contractorName={obj.contractorName}
+              key={index}
+            />
+          )
+        )}
       </ol>
-      <div className="page-navigation govuk-!-padding-bottom-5">
-        {pageNumber > 1 && (
-          <Button
-            label="Previous page"
-            onClick={() => setPageNumber(pageNumber - 1)}
-            type="submit"
-          />
-        )}
-        {pageNumber < totalPages && (
-          <Button
-            label="Next page"
-            onClick={() => setPageNumber(pageNumber + 1)}
-            type="submit"
-            className="right-page-button"
-          />
-        )}
-      </div>
     </div>
   )
 }
