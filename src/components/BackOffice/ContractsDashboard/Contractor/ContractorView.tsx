6ad237b7647cabe5b1aa6c499a021c4da4cc6617
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-06-27 09:46:11 +0100

    create contractor view with active and inactive contracts

---
+++ src/components/BackOffice/ContractsDashboard/Contractor/ContractorView.tsx
@@ -0,0 +1,77 @@
+import { useQuery } from 'react-query'
+import { fetchContracts } from '../../requests'
+
+import Layout from '../../Layout'
+
+import ContractListItems from '../Contract/ContractListItems'
+
+interface ContractorViewProps {
+  contractorReference: string
+  contractorName: string
+}
+
+const ContractorView = ({
+  contractorReference,
+  contractorName,
+}: ContractorViewProps) => {
+  const {
+    data: activeContracts,
+    isLoading: activeContractsLoading,
+    error: activeError,
+  } = useQuery(
+    [
+      'activeContracts',
+      { isActive: true, contractorReference: contractorReference },
+    ],
+    () => fetchContracts(true, contractorReference)
+  )
+
+  const {
+    data: inactiveContracts,
+    isLoading: inactiveContractsLoading,
+    error: inactiveError,
+  } = useQuery(
+    [
+      'inactiveContracts',
+      { isActive: false, contractorReference: contractorReference },
+    ],
+    () => fetchContracts(false, contractorReference)
+  )
+  const activeContractsError = activeError as Error | null
+  const inactiveContractsError = inactiveError as Error | null
+
+  const relativeInactiveContracts = inactiveContracts?.filter(
+    (contract) =>
+      contract.terminationDate > '2020' &&
+      contract.terminationDate < new Date().toISOString()
+  )
+
+  return (
+    <Layout title={`${contractorName}`}>
+      <ol className="lbh-list mobile-working-work-order-list">
+        {activeContracts && (
+          <ContractListItems
+            contracts={activeContracts}
+            heading="Active contracts:"
+            warningText={`No active contracts found for ${contractorName}.`}
+            isLoading={activeContractsLoading}
+            error={activeContractsError}
+          />
+        )}
+      </ol>
+      <ol className="lbh-list mobile-working-work-order-list">
+        {relativeInactiveContracts && (
+          <ContractListItems
+            contracts={relativeInactiveContracts}
+            heading="Inactive contracts:"
+            warningText={`No inactive contracts found for ${contractorName}.`}
+            isLoading={inactiveContractsLoading}
+            error={inactiveContractsError}
+          />
+        )}
+      </ol>
+    </Layout>
+  )
+}
+
+export default ContractorView
