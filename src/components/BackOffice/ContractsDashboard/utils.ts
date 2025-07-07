2e1b09bb0cb05d66f690846deac312d500c5d3d8
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-07-07 15:43:19 +0100

    run lintfix

--- src/components/BackOffice/ContractsDashboard/utils.ts
+++ src/components/BackOffice/ContractsDashboard/utils.ts
@@ -1,11 +1,11 @@
-import Contract from "@/root/src/models/contract"
+import Contract from '@/root/src/models/contract'
 
 export const today = new Date()
 
 export const twoMonthsago = new Date(
-    today.getFullYear(),
-    today.getMonth() - 2,
-    today.getDate()
+  today.getFullYear(),
+  today.getMonth() - 2,
+  today.getDate()
 )
 
 export const oneMonthInTheFuture = new Date(
@@ -26,13 +26,16 @@ export const sixMonthsInTheFuture = new Date(
   today.getDate()
 )
 
-export const filterContractsByExpiryDate = (contracts: Contract[], months: number) => { 
-    const contractExpiryCutOffDate = new Date(
-        today.getFullYear(),
-        today.getMonth() + months,
-        today.getDate()
-    )
-    const filteredContracts = contracts?.filter((contract) => {
+export const filterContractsByExpiryDate = (
+  contracts: Contract[],
+  months: number
+) => {
+  const contractExpiryCutOffDate = new Date(
+    today.getFullYear(),
+    today.getMonth() + months,
+    today.getDate()
+  )
+  const filteredContracts = contracts?.filter((contract) => {
     return (
       new Date(contract.terminationDate) > today &&
       new Date(contract.terminationDate) < contractExpiryCutOffDate
@@ -41,8 +44,11 @@ export const filterContractsByExpiryDate = (contracts: Contract[], months: numbe
   return filteredContracts
 }
 
-export const filterRelativeInactiveContracts = (contracts: Contract[], yearAfter: string) =>  { 
-    const relativeInactiveContracts = contracts?.filter(
+export const filterRelativeInactiveContracts = (
+  contracts: Contract[],
+  yearAfter: string
+) => {
+  const relativeInactiveContracts = contracts?.filter(
     (contract) =>
       contract.terminationDate > yearAfter &&
       contract.terminationDate < new Date().toISOString()
