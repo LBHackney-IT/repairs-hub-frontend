e7c6e1b7180892346f878cbf72ec0401c2d3f45b
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-07-09 14:22:01 +0100

    contractor view render component snapshot test is correct

--- src/components/BackOffice/ContractsDashboard/Contractor/ContractorView.test.js
+++ src/components/BackOffice/ContractsDashboard/Contractor/ContractorView.test.js
@@ -5,28 +5,44 @@ import ContractorView from './ContractorView'
 import {
   mockActiveContracts,
   mockInactiveContracts,
+  mockSorCodeContracts,
 } from '../mockContractsData'
 
 jest.mock('react-query', () => ({
-  useQuery: () => ({
-    data: mockActiveContracts,
-    isLoading: false,
-    error: null,
-  }),
+  useQuery: jest.fn(),
 }))
 
-jest.mock('react-query', () => ({
-  useQuery: () => ({
-    data: mockInactiveContracts,
-    isLoading: false,
-    error: null,
-  }),
-}))
+import { useQuery } from 'react-query'
 
 describe('Contracts dashboard component', () => {
+  beforeEach(() => {
+    useQuery.mockImplementation((key) => {
+      const queryKey = Array.isArray(key) ? key[0] : key
+
+      switch (queryKey) {
+        case 'activeContracts':
+          return {
+            data: mockActiveContracts,
+            isLoading: false,
+            error: null,
+          }
+        case 'inactiveContracts':
+          return {
+            data: mockInactiveContracts,
+            isLoading: false,
+            error: null,
+          }
+        default:
+          return {
+            data: null,
+            isLoading: false,
+            error: null,
+          }
+      }
+    })
+  })
   it('should render the component', async () => {
     const { asFragment } = render(<ContractorView contractorReference="SYC" />)
-
     expect(asFragment()).toMatchSnapshot()
   })
 })
