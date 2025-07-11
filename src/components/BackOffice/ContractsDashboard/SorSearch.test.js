e1f6cec9a5f89b94fc270825525de72c0af46502
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-07-11 12:05:49 +0100

    create snapshot tests for SorSearch

---
+++ src/components/BackOffice/ContractsDashboard/SorSearch.test.js
@@ -0,0 +1,50 @@
+import { render, fireEvent, act, screen } from '@testing-library/react'
+
+import SorSearch from './SorSearch'
+import { mockSorCodeContracts } from './mockContractsData'
+
+describe('SorSearch component', () => {
+  it('should render search bar and title when nothing has been searched', () => {
+    const { asFragment } = render(
+      <SorSearch
+        searchHeadingText={'Check an SOR code'}
+        searchLabelText={`Find out which Sycous Limited contracts an SOR code exists in`}
+        sorCode={''}
+        setSorCode={jest.fn()}
+        isLoading={false}
+        error={null}
+        handleSubmit={jest.fn()}
+        contractorName={'Sycous Limited'}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should render contracts with sor code that has been searched in descending date order', async () => {
+    const descendingDateContractsWithSorCode = [
+      ...mockSorCodeContracts,
+    ].reverse()
+
+    const { asFragment } = render(
+      <SorSearch
+        contracts={descendingDateContractsWithSorCode}
+        searchHeadingText={'Check an SOR code'}
+        searchLabelText={`Find out which Sycous Limited contracts an SOR code exists in`}
+        sorCode={'ABC123'}
+        setSorCode={jest.fn()}
+        isLoading={false}
+        error={null}
+        handleSubmit={jest.fn()}
+        contractorName={'Sycous Limited'}
+      />
+    )
+    await act(async () => {
+      fireEvent.change(screen.getByTestId('input-search'), {
+        target: { value: 'ABC123' },
+      })
+
+      fireEvent.click(screen.getByTestId('submit-search'))
+    })
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
