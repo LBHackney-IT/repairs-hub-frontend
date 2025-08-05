30856f5d5c2fe21ea67ec6b3b59d5a16949ff676
Adam <74552077+adamtry@users.noreply.github.com>
2025-08-05 15:46:17 +0100

    Merge pull request #1167 from LBHackney-IT/fault-tolerant-complete-upload
    Make completeUplaod fault tolerant

--- src/components/WorkOrder/Photos/hooks/uploadFiles/completeUpload.ts
+++ src/components/WorkOrder/Photos/hooks/uploadFiles/completeUpload.ts
@@ -1,4 +1,5 @@
 import { frontEndApiRequest } from '@/root/src/utils/frontEndApiClient/requests'
+import faultTolerantRequest from './faultTolerantRequest'
 
 const completeUpload = async (
   workOrderReference: string,
@@ -6,7 +7,7 @@ const completeUpload = async (
   description: string,
   uploadGroupLabel: string
 ) => {
-  try {
+  const requestOperation = async () => {
     const result = await frontEndApiRequest({
       method: 'post',
       path: `/api/workOrders/images/completeUpload`,
@@ -17,19 +18,19 @@ const completeUpload = async (
         uploadGroupLabel,
       },
     })
-
-    return { success: true, result }
-  } catch (error) {
-    let errorMessage = ''
-
-    if (typeof error == 'string') {
-      errorMessage = error
-    } else {
-      errorMessage = error.message
-    }
-
-    return { success: false, error: errorMessage }
+    return result
   }
+
+  const faultTolerantResult = await faultTolerantRequest(requestOperation, 5)
+
+  if (!faultTolerantResult.success) {
+    return {
+      success: false,
+      error: faultTolerantResult.error?.message || 'Request failed',
+    }
+  }
+
+  return { success: true, result: faultTolerantResult.data }
 }
 
 export default completeUpload
