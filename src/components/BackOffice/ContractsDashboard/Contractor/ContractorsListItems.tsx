2a038d841bdc1fb84a8b0bdb3fd9496ac1eda284
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-08-06 16:25:19 +0100

    refactor

--- src/components/BackOffice/ContractsDashboard/Contractor/ContractorsListItems.tsx
+++ src/components/BackOffice/ContractsDashboard/Contractor/ContractorsListItems.tsx
@@ -1,45 +1,15 @@
-import Contract from '@/root/src/models/contract'
+import Contractor from '@/root/src/models/contractor'
 import ContractorListItem from './ContractorListItem'
 import WarningInfoBox from '../../../Template/WarningInfoBox'
 
-import {
-  filterRelevantContracts,
-  mapContractorNamesAndReferences,
-  sortContractorNamesAndReferencesByContractorName,
-} from '../utils'
-
 interface ContractorsListItemsProps {
-  contracts: Contract[]
+  contractors: Contractor[]
 }
 
-const ContractorsListItems = ({ contracts }: ContractorsListItemsProps) => {
-  const relevantContracts = filterRelevantContracts(contracts, '2020')
-
-  const contractorNames = new Set<string>(
-    relevantContracts.map((contract) => contract.contractorName)
-  )
-  const contractorReferences = new Set<string>(
-    relevantContracts.map((contract) => contract.contractorReference)
-  )
-
-  const contractorNamesAndReferences = mapContractorNamesAndReferences(
-    contractorNames,
-    contractorReferences
-  )
-
-  const contractorNamesAndReferencesSortedByContractorName = sortContractorNamesAndReferencesByContractorName(
-    contractorNamesAndReferences
-  )
-
-  if (
-    !contractorNamesAndReferencesSortedByContractorName ||
-    Object.keys(contractorNamesAndReferencesSortedByContractorName).length === 0
-  ) {
+const ContractorsListItems = ({ contractors }: ContractorsListItemsProps) => {
+  if (contractors.length === 0) {
     return (
       <>
-        <h3 className="lbh-heading-h3 lbh-!-font-weight-bold govuk-!-margin-bottom-1">
-          Contractors:
-        </h3>
         <div style={{ width: '85%' }}>
           <WarningInfoBox
             header="No contractors found!"
@@ -53,22 +23,18 @@ const ContractorsListItems = ({ contracts }: ContractorsListItemsProps) => {
 
   return (
     <div>
-      <h3 className="lbh-heading-h3 lbh-!-font-weight-bold govuk-!-margin-bottom-1">
-        Contractors:
-      </h3>
       <ol
         className="lbh-list mobile-working-work-order-list"
         data-test-id="contractors-list"
       >
-        {contractorNamesAndReferencesSortedByContractorName.map(
-          (obj, index) => (
-            <ContractorListItem
-              contractorReference={obj.contractorReference}
-              contractorName={obj.contractorName}
-              key={index}
-            />
-          )
-        )}
+        {contractors.map((obj, index) => (
+          <ContractorListItem
+            contractorReference={obj.contractorReference}
+            contractorName={obj.contractorName}
+            activeContractCount={obj.activeContractCount}
+            key={index}
+          />
+        ))}
       </ol>
     </div>
   )
