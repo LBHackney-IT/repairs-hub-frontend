935dfe67f38b82ac7f8a199d7e9ad981834a4178
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-08-06 16:46:01 +0100

    update snapshots and test

---
+++ src/components/BackOffice/ContractsDashboard/mockContractorData.js
@@ -0,0 +1,27 @@
+export const mockContractors = [
+  {
+    contractorReference: 'NOO',
+    contractorName: 'No ltd',
+    activeContractCount: 0,
+  },
+  {
+    contractorReference: 'YES',
+    contractorName: 'Yes ltd',
+    activeContractCount: 1,
+  },
+  {
+    contractorReference: 'SHE',
+    contractorName: 'She ltd',
+    activeContractCount: 0,
+  },
+  {
+    contractorReference: 'CAT',
+    contractorName: 'Cat ltd',
+    activeContractCount: 0,
+  },
+  {
+    contractorReference: 'DOG',
+    contractorName: 'Dog ltd',
+    activeContractCount: 1,
+  },
+]
