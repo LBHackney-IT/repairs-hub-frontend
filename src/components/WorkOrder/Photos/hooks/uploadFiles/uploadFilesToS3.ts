b8c65414feed61886d566100ecb45ad21a32c7dc
Adam <74552077+adamtry@users.noreply.github.com>
2025-08-11 14:59:59 +0100

    Compress files in series, better error message (#1176)
    * Compress files in series, better error message
    
    * Refactor file upload process to handle compression errors and ensure all files are uploaded
    
    * Contracts snapshot
    
    * Cleanup logic
    
    * Better message
    
    * Fix snapshot?
    
    * Separate compress and upload status logging

--- src/components/WorkOrder/Photos/hooks/uploadFiles/uploadFilesToS3.ts
+++ src/components/WorkOrder/Photos/hooks/uploadFiles/uploadFilesToS3.ts
@@ -1,70 +1,60 @@
+import axios from 'axios'
 import { Link } from '../types'
-import { compressFile } from './compressFile'
 import faultTolerantRequest from './faultTolerantRequest'
-import uploadFileToS3 from './uploadFileToS3'
 
 const uploadFilesToS3 = async (
   files: File[],
   links: Link[],
   fileUploadCompleteCallback: () => void
-): Promise<{ success: boolean; error?: any }> => {
-  const promiseList = files.map((file, i) => {
-    return uploadWrapper(file, links[i])
-  })
-
-  // wrap with callback to track how many have been completed
-  const wrappedPromises = promiseList.map((promise) =>
-    promise.then((result) => {
-      fileUploadCompleteCallback()
-      return result
-    })
-  )
-
+): Promise<{ success: boolean; error?: unknown }> => {
+  // Upload in parallel for faster performance on good internet connections
   try {
-    const results = await Promise.allSettled(wrappedPromises)
-
-    const successfulResults = results.filter(
-      (x) => x.status === 'fulfilled' && x.value.success
+    await Promise.all(
+      files.map(async (compressedFile, i) => {
+        await uploadFileToS3(compressedFile, links[i])
+        fileUploadCompleteCallback()
+      })
     )
-
-    const allSuccessed = successfulResults.length === results.length
-
-    if (allSuccessed) return { success: true }
-
+  } catch (error) {
     return {
       success: false,
-      error: 'Some photos failed to upload. Please try again',
+      error: 'Some photos failed to upload. Please try again. ' + error,
     }
-  } catch (error) {
-    return { success: false, error }
   }
+
+  return { success: true }
 }
 
-const uploadWrapper = async (
+const uploadFileToS3 = async (
   file: File,
   link: Link
-): Promise<{
-  success: boolean
-  error?: unknown
-}> => {
+): Promise<{ success: boolean; error?: unknown }> => {
   // Need to destructure because these are non-enumerable properties
   const { name, size, type } = file
   const fileDetails = { name, size, type }
 
   console.log('preparing to upload file', JSON.stringify(fileDetails))
-  const compressResult = await compressFile(file)
-  const result = await faultTolerantRequest(
-    async () =>
-      await uploadFileToS3(compressResult.file, link).catch((err) => {
+  const result = await faultTolerantRequest(() =>
+    axios
+      .request({
+        method: 'PUT',
+        url: link.presignedUrl,
+        data: file,
+      })
+      .catch((err) => {
         console.error(
           'uploadFileToS3 failed for file',
           JSON.stringify(fileDetails),
           'with error',
           JSON.stringify({ name: err.name, message: err.message })
         )
-        throw err
+        throw Error('Failed to upload file: ' + err)
       })
   )
+
+  if (!result.success) {
+    throw result.error
+  }
 
   return result
 }
