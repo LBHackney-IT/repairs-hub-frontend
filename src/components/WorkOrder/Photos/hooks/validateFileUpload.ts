91b05e28b5983c14ccc9c37f0d99628b609dcfbb
Adam <adam.tracy20@gmail.com>
2025-09-04 09:36:29 +0100

    Typescript

---
+++ src/components/WorkOrder/Photos/hooks/validateFileUpload.ts
@@ -0,0 +1,28 @@
+import { filesize } from 'filesize'
+
+const allowedFileTypes = new Set(['image/png', 'image/jpeg'])
+const MAX_FILE_SIZE = 20_000_000
+const MAX_FILE_COUNT = 10
+
+const validateFileUpload = (files) => {
+  if (files.length === 0) return null
+
+  if (files.length > MAX_FILE_COUNT) {
+    return `You cannot attach more than ${MAX_FILE_COUNT} photos`
+  }
+
+  for (const file of files) {
+    if (!allowedFileTypes.has(file.type)) {
+      return `Unsupported file type "${file.type}". Allowed types: PNG & JPG`
+    }
+
+    //20mb
+    if (file.size > MAX_FILE_SIZE) {
+      return `Filesize cannot exceed ${filesize(MAX_FILE_SIZE)}`
+    }
+  }
+
+  return null
+}
+
+export default validateFileUpload
