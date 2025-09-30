adc8889d0336382ae9d6cb1be83d360a4a488981
Adam <74552077+adamtry@users.noreply.github.com>
2025-09-30 14:51:04 +0100

    Try fix logging (#1230)

--- src/pages/api/logging.ts
+++ src/pages/api/logging.ts
@@ -1,8 +1,8 @@
 import { StatusCodes } from 'http-status-codes'
-import logger from 'loglevel'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  logger.info(req.body)
+  const message = req.body?.message || JSON.stringify(req.body)
+  console.log(message)
   res.status(StatusCodes.NO_CONTENT).end()
 }
