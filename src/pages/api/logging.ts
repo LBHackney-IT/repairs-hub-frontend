e6b907655575f1e5d1b95da455d8fe6935c8f210
Adam <adam.tracy20@gmail.com>
2025-10-01 09:47:24 +0100

    Try to fix dev logging

--- src/pages/api/logging.ts
+++ src/pages/api/logging.ts
@@ -1,8 +1,14 @@
 import { StatusCodes } from 'http-status-codes'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const message = req.body?.message || JSON.stringify(req.body)
-  console.log(message)
+
+  // Log the message
+  console.log('CLOUDWATCH_LOG:', message)
+
+  // Force console flush before responding
+  await new Promise((resolve) => process.nextTick(resolve))
+
   res.status(StatusCodes.NO_CONTENT).end()
 }
