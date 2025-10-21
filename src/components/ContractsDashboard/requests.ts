e6392c969f2319b1f710ce58c2db58891dfa5d46
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-10-21 14:59:46 +0100

    move request functions to contracts dashboard

---
+++ src/components/ContractsDashboard/requests.ts
@@ -0,0 +1,61 @@
+import { frontEndApiRequest } from '../../utils/frontEndApiClient/requests'
+import Contract from '@/models/contract'
+import Contractor from '@/models/contractor'
+
+interface FetchContractorsArguments {
+  contractsExpiryFilterDate: Date
+}
+
+export const contractsDashboardFetchContractors = async ({
+  contractsExpiryFilterDate,
+}: FetchContractorsArguments): Promise<Contractor[] | null> => {
+  const params = {}
+  if (contractsExpiryFilterDate !== null)
+    params['contractsExpiryFilterDate'] = contractsExpiryFilterDate
+
+  const contractors = await frontEndApiRequest({
+    method: 'get',
+    path: '/api/backoffice/contractors?',
+    params: params,
+  })
+
+  return contractors
+}
+
+interface FetchContractsArguments {
+  isActive: boolean | undefined
+  contractorReference: string | undefined
+  sorCode: string | undefined
+}
+
+export const fetchContracts = async ({
+  isActive,
+  contractorReference,
+  sorCode,
+}: FetchContractsArguments): Promise<Contract[] | null> => {
+  const params = {}
+
+  if (typeof isActive === 'boolean') params['isActive'] = isActive
+  if (contractorReference !== null && contractorReference !== '')
+    params['contractorReference'] = contractorReference
+  if (sorCode !== null && sorCode !== '') params['sorCode'] = sorCode
+
+  const contracts = await frontEndApiRequest({
+    method: 'get',
+    path: `/api/backoffice/contracts?`,
+    params: params,
+  })
+
+  return contracts
+}
+
+export const fetchContract = async (
+  contractReference: string
+): Promise<Contract | null> => {
+  const contract = await frontEndApiRequest({
+    method: 'get',
+    path: `/api/backoffice/contract/${contractReference}`,
+  })
+
+  return contract
+}
