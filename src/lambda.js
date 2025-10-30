bb00980a4b39ea1967594223c0cc2f19cd739538
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-10-30 15:07:11 +0000

    ghdfgdfgdgd

--- src/lambda.js
+++ src/lambda.js
@@ -8,7 +8,7 @@ AWSXRay.enableAutomaticMode()
 const standaloneDir = path.join(__dirname, '../build/_next/standalone')
 const Next = require(path.join(standaloneDir, 'node_modules/next'))
 
-const app = require('next')({
+const app = Next({
   dev: false,
   dir: standaloneDir,
   conf: {
