23c0aa5b9e2a8bb9c62a1c883fff4d7d3873f2c1
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-10-30 14:04:30 +0000

    maybe maybe

--- src/lambda.js
+++ src/lambda.js
@@ -34,10 +34,7 @@ async function init() {
 
 server.use(AWSXRay.express.openSegment('NextJSApp'))
 
-server.use(
-  '/_next/static',
-  files(path.join(__dirname, '../build/_next/static'))
-)
+server.use(files(path.join(__dirname, '../build/_next/static')))
 server.use(files(path.join(__dirname, '../public')))
 
 server.all('*', async (req, res) => {
