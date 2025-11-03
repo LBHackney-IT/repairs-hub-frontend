be559fdb19f3afee2b9d0d43581dd70fd91fd8bc
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-11-03 15:32:48 +0000

    Add temp logging

--- src/lambda.js
+++ src/lambda.js
@@ -1,6 +1,7 @@
 const server = require('restana')()
 const files = require('serve-static')
 const path = require('path')
+const fs = require('fs')
 // const AWSXRay = require('aws-xray-sdk')
 
 // AWSXRay.enableAutomaticMode()
@@ -13,6 +14,31 @@ const { config } = require(path.join(
   'build/_next/required-server-files.json'
 ))
 process.env.__NEXT_PRIVATE_STANDALONE_CONFIG = JSON.stringify(config)
+
+console.log('=== FILESYSTEM CHECK ===')
+console.log('__dirname:', __dirname)
+
+const staticDir = path.join(__dirname, '../build/_next/static')
+console.log('Static dir path:', staticDir)
+console.log('Static dir exists:', fs.existsSync(staticDir))
+
+if (fs.existsSync(staticDir)) {
+  console.log('Static dir contents:', fs.readdirSync(staticDir))
+
+  const chunksDir = path.join(staticDir, 'chunks')
+  if (fs.existsSync(chunksDir)) {
+    console.log('Chunks dir exists!')
+    const chunks = fs.readdirSync(chunksDir)
+    console.log('Number of chunks:', chunks.length)
+    console.log('First 10 chunks:', chunks.slice(0, 10))
+
+    // Check for specific file
+    const webpackFile = chunks.find((f) => f.includes('webpack'))
+    console.log('Found webpack file:', webpackFile)
+  } else {
+    console.log('Chunks dir does NOT exist')
+  }
+}
 
 const app = Next({
   dev: false,
@@ -45,18 +71,15 @@ server.use(
   files(path.join(__dirname, '../build/_next/static'))
 )
 
-const staticHandler = files(path.join(__dirname, '../build/_next/static'))
-server.use('/_next/static', (req, res, next) => {
-  console.log(
-    'Attempting to serve from:',
-    path.join(__dirname, '../build/_next/static')
-  )
-  console.log('Looking for file:', req.url)
-  return staticHandler(req, res, (err) => {
-    if (err) console.log('Static handler error:', err)
-    console.log('Static handler did NOT find file, passing to next()')
-    next()
-  })
+server.use((req, res, next) => {
+  // Manually handle /_next/static requests
+  if (req.url.startsWith('/_next/static')) {
+    const filePath = path.join(__dirname, '../build', req.url)
+    console.log('Trying to serve:', filePath)
+    return files(path.join(__dirname, '../build'))(req, res, next)
+  }
+
+  return next()
 })
 
 server.use(files(path.join(__dirname, '../public')))
