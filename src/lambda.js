02980f5a5e718bd236641ddd03f5e91d12392821
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-11-04 14:46:07 +0000

    RM more xray

--- src/lambda.js
+++ src/lambda.js
@@ -1,9 +1,6 @@
 const server = require('restana')()
 const files = require('serve-static')
 const path = require('path')
-const AWSXRay = require('aws-xray-sdk')
-
-AWSXRay.enableAutomaticMode()
 
 const standaloneDir = path.join(__dirname, '../build/_next/standalone')
 const Next = require(path.join(standaloneDir, 'node_modules/next'))
@@ -39,7 +36,5 @@ server.all('*', async (req, res) => {
   await init()
   return nextRequestHandler(req, res)
 })
-
-server.use(AWSXRay.express.closeSegment())
 
 module.exports.handler = require('serverless-http')(server)
