cee1991bf8f875d126acc1c758c4f9ebd4a58d95
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2025-11-05 15:00:45 +0000

    Update more tests

--- src/hooks/useClickOutside.test.js
+++ src/hooks/useClickOutside.test.js
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import useClickOutside from './useClickOutside'
-import { render, screen } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 const mockHandler = jest.fn()
@@ -18,16 +18,16 @@ const MockComponent = () => {
   )
 }
 
-describe('useClickOutside', () => {
+describe('useClickOutside', async () => {
   it('does nothing when the click is inside the element', () => {
     render(<MockComponent />)
-    userEvent.click(screen.getByText('Inside text'))
+    await act(() => userEvent.click(screen.getByText('Inside text')))
     expect(mockHandler).not.toHaveBeenCalled()
   })
 
-  it('fires the handler when there is a click outside the element', () => {
+  it('fires the handler when there is a click outside the element', async () => {
     render(<MockComponent />)
-    userEvent.click(screen.getByText('Outside text'))
+    await act(() => userEvent.click(screen.getByText('Outside text')))
     expect(mockHandler).toHaveBeenCalledTimes(1)
   })
 })
